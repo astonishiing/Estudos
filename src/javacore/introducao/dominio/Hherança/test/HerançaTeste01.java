931d5e2d861742293885f46b224bad706e8f7aaf
astonishiing <wcardoso4510@gmail.com>
2025-03-10 17:28:10 -0300

    criacao do executavel

---
+++ src/javacore/introducao/dominio/Hherança/test/HerançaTeste01.java
@@ -0,0 +1,11 @@
+package javacore.introducao.dominio.Hherança.test;
+
+import javacore.introducao.dominio.Hherança.dominio.Endereco;
+
+public class HerançaTeste01 {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("");
+    }
+}
