266964defa39d2523d2d713e4dd75f904d92de78
astonishiing <wcardoso4510@gmail.com>
2025-03-10 17:26:43 -0300

    adicao dos getters e setters e remoção dos contrutores

--- src/javacore/introducao/dominio/Hherança/dominio/Pessoa.java
+++ src/javacore/introducao/dominio/Hherança/dominio/Pessoa.java
@@ -2,5 +2,30 @@ package javacore.introducao.dominio.Hherança.dominio;
 
 public class Pessoa {
     private String nome;
+    private String cof;
+    private Endereco endereco;
 
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCof() {
+        return cof;
+    }
+
+    public void setCof(String cof) {
+        this.cof = cof;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 }
