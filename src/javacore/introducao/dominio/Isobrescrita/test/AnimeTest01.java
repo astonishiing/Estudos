08fd03470616bf71fdb39b741931b6318517c901
astonishiing <wcardoso4510@gmail.com>
2025-03-11 09:37:05 -0300

    criacao de sobrescrita com toString

---
+++ src/javacore/introducao/dominio/Isobrescrita/test/AnimeTest01.java
@@ -0,0 +1,10 @@
+package javacore.introducao.dominio.Isobrescrita.test;
+
+import javacore.introducao.dominio.Isobrescrita.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Dr. Stone");
+        System.out.println(anime);
+    }
+}
