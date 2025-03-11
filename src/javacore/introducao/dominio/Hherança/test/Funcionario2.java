07a7d8d01fcfb5cd6f83ab8f03b886dca119429b
astonishiing <wcardoso4510@gmail.com>
2025-03-10 23:18:51 -0300

    Funcionario

---
+++ src/javacore/introducao/dominio/Hherança/test/Funcionario2.java
@@ -0,0 +1,9 @@
+package javacore.introducao.dominio.Hherança.test;
+
+import javacore.introducao.dominio.Hherança.dominio.Pessoa;
+
+public class Funcionario2 extends Pessoa {
+    public void imprime(){
+        this.nome = "sasa";
+    }
+}
