2c500ea5bccd659ba449ec4b705be2f9b69328fb
astonishiing <wcardoso4510@gmail.com>
2025-03-13 17:44:50 -0300

    Criacao da enumeracao

--- src/javacore/introducao/dominio/Knum/test/ClienteTeste01.java
+++ src/javacore/introducao/dominio/Knum/test/ClienteTeste01.java
@@ -1,4 +1,13 @@
 package javacore.introducao.dominio.Knum.test;
 
-public class teste {
+import javacore.introducao.dominio.Knum.dominio.Cliente;
+
+public class ClienteTeste01 {
+    Cliente cliente1 = new Cliente("Tsubasa", "PESSOA FISICA");
+    Cliente cliente2 = new Cliente("Tsubasa", "PESSOA JURIDICA");
+    Cliente cliente4 = new Cliente("Tsubasa", "pessoa fisica");
+    Cliente cliente3 = new Cliente("Tsubasa", "pessoa Juridica");
+
+
+
 }
