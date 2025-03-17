97b688681feee881aaa8042844380d6899214e8b
astonishiing <wcardoso4510@gmail.com>
2025-03-17 14:41:14 -0300

    Criacao de uma classe abstrata

---
+++ src/javacore/introducao/dominio/Lclassesabstratas/dominio/Gerente.java
@@ -0,0 +1,7 @@
+package javacore.introducao.dominio.Lclassesabstratas.dominio;
+
+public class Gerente extends Funcionario {
+    public Gerente(String nome, double salario) {
+        super(nome, salario);
+    }
+}
---
+++ src/javacore/introducao/dominio/Lclassesabstratas/test/FuncionarioTeste01.java
@@ -0,0 +1,14 @@
+package javacore.introducao.dominio.Lclassesabstratas.test;
+
+import javacore.introducao.dominio.Lclassesabstratas.dominio.Funcionario;
+import javacore.introducao.dominio.Lclassesabstratas.dominio.Gerente;
+
+public class FuncionarioTeste01 {
+    public static void main(String[] args) {
+        //Funcionario funcionario = new Funcionario("Zoro", 2000); // Nesse caso não deveria existir apenas 1 funcionario
+        Gerente gerente = new Gerente("Nami", 5000);
+        //System.out.println(funcionario);
+        System.out.println(gerente);
+    }
+
+}
