84d39400438f9641cd796edaf7a6887c60cac298
astonishiing <wcardoso4510@gmail.com>
2025-03-17 14:47:53 -0300

    Criacao da classe Desenvolvedor

--- src/javacore/introducao/dominio/Lclassesabstratas/dominio/Desenvolvedor.java
+++ src/javacore/introducao/dominio/Lclassesabstratas/dominio/Desenvolvedor.java
@@ -1,4 +1,15 @@
 package javacore.introducao.dominio.Lclassesabstratas.dominio;
 
-public class Desenvolvedor {
+public class Desenvolvedor extends Funcionario {
+    public Desenvolvedor(String nome, double salario){
+        super(nome, salario);
+    }
+
+    @Override
+    public String toString() {
+        return "Desenvolvedor{" +
+                "nome='" + nome + '\'' +
+                ", salario=" + salario +
+                '}';
+    }
 }
--- src/javacore/introducao/dominio/Lclassesabstratas/test/FuncionarioTeste01.java
+++ src/javacore/introducao/dominio/Lclassesabstratas/test/FuncionarioTeste01.java
@@ -1,5 +1,6 @@
 package javacore.introducao.dominio.Lclassesabstratas.test;
 
+import javacore.introducao.dominio.Lclassesabstratas.dominio.Desenvolvedor;
 import javacore.introducao.dominio.Lclassesabstratas.dominio.Funcionario;
 import javacore.introducao.dominio.Lclassesabstratas.dominio.Gerente;
 
@@ -7,8 +8,10 @@ public class FuncionarioTeste01 {
     public static void main(String[] args) {
         //Funcionario funcionario = new Funcionario("Zoro", 2000); // Nesse caso não deveria existir apenas 1 funcionario
         Gerente gerente = new Gerente("Nami", 5000);
+        Desenvolvedor desenvolvedor = new Desenvolvedor("Toya", 12000);
         //System.out.println(funcionario);
         System.out.println(gerente);
+        System.out.println(desenvolvedor);
     }
 
 }
