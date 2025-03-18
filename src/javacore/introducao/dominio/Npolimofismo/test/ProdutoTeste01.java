b3430fed23df236acd0743cfe8b15ce4387dfd86
astonishiing <wcardoso4510@gmail.com>
2025-03-18 10:52:41 -0300

    Parametros polimorficos

--- src/javacore/introducao/dominio/Npolimofismo/test/ProdutoTeste01.java
+++ src/javacore/introducao/dominio/Npolimofismo/test/ProdutoTeste01.java
@@ -1,6 +1,7 @@
 package javacore.introducao.dominio.Npolimofismo.test;
 
 import javacore.introducao.dominio.Npolimofismo.dominio.Computador;
+import javacore.introducao.dominio.Npolimofismo.dominio.Televisao;
 import javacore.introducao.dominio.Npolimofismo.dominio.Tomate;
 import javacore.introducao.dominio.Npolimofismo.servico.CalculadoraImposto;
 
@@ -8,9 +9,12 @@ public class ProdutoTeste01 {
     public static void main(String[] args) {
         Computador computador = new Computador("NUC0i7", 11000);
         Tomate tomate = new Tomate("Tomate Siciliano", 10);
+        Televisao tv = new Televisao("Samsung 50\"", 5000);
         CalculadoraImposto.calcularImposto(computador);
         System.out.println("-------------");
         CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("-------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 
 }
