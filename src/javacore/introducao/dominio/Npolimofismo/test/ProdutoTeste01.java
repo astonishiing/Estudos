2bbd051d3d0f50ed98fc08dc4ccf8c169184f819
astonishiing <wcardoso4510@gmail.com>
2025-03-18 10:07:25 -0300

    Criacao do Executavel

--- src/javacore/introducao/dominio/Npolimofismo/test/ProdutoTeste01.java
+++ src/javacore/introducao/dominio/Npolimofismo/test/ProdutoTeste01.java
@@ -1,4 +1,16 @@
 package javacore.introducao.dominio.Npolimofismo.test;
 
+import javacore.introducao.dominio.Npolimofismo.dominio.Computador;
+import javacore.introducao.dominio.Npolimofismo.dominio.Tomate;
+import javacore.introducao.dominio.Npolimofismo.servico.CalculadoraImposto;
+
 public class ProdutoTeste01 {
+    public static void main(String[] args) {
+        Computador computador = new Computador("NUC0i7", 11000);
+        Tomate tomate = new Tomate("Tomate Siciliano", 10);
+        CalculadoraImposto.calcularImpostoComputador(computador);
+        System.out.println("-------------");
+        CalculadoraImposto.calcularImpostoTomate(tomate);
+    }
+
 }
