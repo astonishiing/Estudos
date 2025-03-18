cfc0dd40b25d119adeb5d920748c3fc99c8b05b3
astonishiing <wcardoso4510@gmail.com>
2025-03-18 10:39:21 -0300

    Criacao do segundo executavel para funcionalidades

--- src/javacore/introducao/dominio/Npolimofismo/test/ProdutoTeste02.java
+++ src/javacore/introducao/dominio/Npolimofismo/test/ProdutoTeste02.java
@@ -1,4 +1,19 @@
 package javacore.introducao.dominio.Npolimofismo.test;
 
+import javacore.introducao.dominio.Npolimofismo.dominio.Computador;
+import javacore.introducao.dominio.Npolimofismo.dominio.Produto;
+import javacore.introducao.dominio.Npolimofismo.dominio.Tomate;
+
 public class ProdutoTeste02 {
+    public static void main(String[] args) {
+        Produto produto = new Computador("Ryzen 9", 3000);
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+        System.out.println("------------");
+        Produto produto2 = new Tomate("Americano", 20);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
+        System.out.println(produto2.calcularImposto());
+    }
 }
