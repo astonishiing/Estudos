35e5adf9e4f6b70150d0e231e826195beff51b11
astonishiing <wcardoso4510@gmail.com>
2025-03-18 09:47:55 -0300

    Adicionando os getters

---
+++ src/javacore/introducao/dominio/Npolimofismo/servico/CalculaddoraImposto.java
@@ -0,0 +1,10 @@
+package javacore.introducao.dominio.Npolimofismo.servico;
+
+import javacore.introducao.dominio.Npolimofismo.dominio.Computador;
+
+public class CalculaddoraImposto {
+    public void calcularImpostoComputador(Computador computador){
+        System.out.println("Relatório de imposto do computador");
+        computador
+    }
+}
