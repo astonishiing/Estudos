9f51dd8c74b2d631294f8563df5df8d263749863
astonishiing <wcardoso4510@gmail.com>
2025-04-17 14:53:46 -0300

    Criacao das classes e insercao do nome em estudante02

--- src/javaintroducao/Aintroducaoclasses/dominio/Estudante02.java
+++ src/javaintroducao/Aintroducaoclasses/dominio/Estudante02.java
@@ -2,6 +2,6 @@ package javaintroducao.Aintroducaoclasses.dominio;
 
 public class Estudante02 {
     public String nome = "Zoro";
-    public int idade = ;
-    public char sexo = ;
+    public int idade;
+    public char sexo;
 }
--- src/javaintroducao/Aintroducaoclasses/test/EstudanteTest02.java
+++ src/javaintroducao/Aintroducaoclasses/test/EstudanteTest02.java
@@ -1,4 +1,22 @@
 package javaintroducao.Aintroducaoclasses.test;
 
+import javaintroducao.Aintroducaoclasses.dominio.Estudante02;
+
 public class EstudanteTest02 {
+    public static void main(String[] args) {
+        Estudante02 estudante02 = new Estudante02();
+        Estudante02 estudante03 = new Estudante02();
+
+        estudante02.nome = "Sanji";
+
+        System.out.println(estudante02.nome);
+        System.out.println(estudante02.idade);
+        System.out.println(estudante02.sexo);
+        System.out.println("---------------");
+
+        System.out.println(estudante03.nome);
+        System.out.println(estudante03.idade);
+        System.out.println(estudante03.sexo);
+
+    }
 }
