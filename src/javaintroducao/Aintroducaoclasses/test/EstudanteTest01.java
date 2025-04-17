c3700046c2381b2b76e2abcc498ed4b155f736aa
astonishiing <wcardoso4510@gmail.com>
2025-04-17 14:38:46 -0300

    impressão

--- src/javaintroducao/Aintroducaoclasses/test/EstudanteTest01.java
+++ src/javaintroducao/Aintroducaoclasses/test/EstudanteTest01.java
@@ -8,5 +8,9 @@ public class EstudanteTest01 {
         estudante.nome = "Luffy";
         estudante.idade = 21;
         estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
     }
 }
