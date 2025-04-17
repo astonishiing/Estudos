5994ceab6fa322be74c38e9a4f7980ca5143165d
astonishiing <wcardoso4510@gmail.com>
2025-04-17 14:37:58 -0300

    Criacao de classes com objetos

---
+++ src/javaintroducao/Aintroducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,12 @@
+package javaintroducao.Aintroducaoclasses.test;
+
+import javaintroducao.Aintroducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+    }
+}
