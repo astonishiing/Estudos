fb8c45d41889afd2771da5043e0afeabab22dba1
astonishiing <wcardoso4510@gmail.com>
2025-04-23 17:49:07 -0300

    O uso do this em construtore

--- src/javacore/introducao/dominio/Dconstrutores/dominio/Anime.java
+++ src/javacore/introducao/dominio/Dconstrutores/dominio/Anime.java
@@ -7,7 +7,8 @@ public class Anime {
     private String genero;
     private String estudio;
 
-    //Construtores são relacionados ao objeto, então é por isso que utilizamos o this(), sem parâmetros
+    //Construtores são relacionados ao objeto, então é por isso que utilizado o this(), sem parâmetros
+    // O this so pode ser utilizado dessa forma somente em construtores
     public Anime(String nome, String tipo, int episodios, String genero){
         this(); // o this, nesse caso so pode ser utilizado dentro de contrutores e precisa obrigatoriamente ser a primeira linha executada
         this.nome = nome;
