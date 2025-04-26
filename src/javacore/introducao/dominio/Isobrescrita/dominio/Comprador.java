602006370395d08fd3fc7822d39f4cff974c6434
astonishiing <wcardoso4510@gmail.com>
2025-04-26 18:21:25 -0300

    Melhoria na explicação

--- src/javacore/introducao/dominio/Isobrescrita/dominio/Comprador.java
+++ src/javacore/introducao/dominio/Isobrescrita/dominio/Comprador.java
@@ -3,7 +3,6 @@ package javacore.introducao.dominio.Isobrescrita.dominio;
 public class Comprador {
     private String nome;
 
-
     public String getNome() {
         return nome;
     }
--- src/javacore/introducao/dominio/Isobrescrita/test/AnimeTest01.java
+++ src/javacore/introducao/dominio/Isobrescrita/test/AnimeTest01.java
@@ -5,7 +5,6 @@ import javacore.introducao.dominio.Isobrescrita.dominio.Anime;
 public class AnimeTest01 {
     public static void main(String[] args) {
         Anime anime = new Anime("Dr. Stone");
-
         System.out.println(anime.toString());
         //Nesse caso não ha a necessida de chamar .toString(), pois a propria IDE faz isso para você de forma implicita
 
