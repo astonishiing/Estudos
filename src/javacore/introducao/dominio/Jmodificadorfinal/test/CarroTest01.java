febee55f217cd4fd0c0069f4a81ddaaf42522ef7
astonishiing <wcardoso4510@gmail.com>
2025-05-03 15:11:43 -0300

    Explicação

--- src/javacore/introducao/dominio/Jmodificadorfinal/test/CarroTest01.java
+++ src/javacore/introducao/dominio/Jmodificadorfinal/test/CarroTest01.java
@@ -7,7 +7,7 @@ import javacore.introducao.dominio.Jmodificadorfinal.dominio.Ferrari;
 public class CarroTest01 {
     public static void main(String[] args) {
         Carro carro = new Carro();
-        Comprador comprador2 = new Comprador();
+        //Comprador comprador2 = new Comprador();
         //carro.Comprador = comprador2 > Nesse caso nao pode mudar a referência do objeto, nem mesmo criando um novo objeto
 
         System.out.println(Carro.VELOCIDADE_LIMITE);
