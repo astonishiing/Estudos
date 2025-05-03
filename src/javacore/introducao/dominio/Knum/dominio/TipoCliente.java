90c5a1dd51793c020e0a5fef553994018fbd9840
astonishiing <wcardoso4510@gmail.com>
2025-05-03 15:02:59 -0300

    Melhoria

--- src/javacore/introducao/dominio/Knum/dominio/TipoCliente.java
+++ src/javacore/introducao/dominio/Knum/dominio/TipoCliente.java
@@ -3,11 +3,11 @@ package javacore.introducao.dominio.Knum.dominio;
 public enum TipoCliente {
     PESSOA_FISICA(1, "Pessoa Fisica"),
     PESSOA_JURIDICA(2, "Pessoa Juridica");
-    private int valor;
+    public final int VALOR;
     private String nomeRelatorio;
 
     TipoCliente(int valor, String nomeRelatorio){
-        this.valor = valor;
+        this.VALOR = valor;
         this.nomeRelatorio = nomeRelatorio;
     }
 
@@ -22,8 +22,5 @@ public enum TipoCliente {
 
     public String getNomeRelatorio() {
         return nomeRelatorio;
-    }
-    public int getValor(){
-        return valor;
     }
 }
