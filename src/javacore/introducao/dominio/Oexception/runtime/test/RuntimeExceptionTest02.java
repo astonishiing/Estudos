607497c5786e50c2a0d32b96db65dcfbff62250d
astonishiing <wcardoso4510@gmail.com>
2025-05-05 17:22:17 -0300

    RuntimeExceptionTest02

--- src/javacore/introducao/dominio/Oexception/runtime/test/RuntimeExceptionTest02.java
+++ src/javacore/introducao/dominio/Oexception/runtime/test/RuntimeExceptionTest02.java
@@ -5,12 +5,15 @@ public class RuntimeExceptionTest02 {
         divisao(1, 0);
     }
     private static int divisao(int a, int b){
+        if(b == 0){
+            throw new IllegalArgumentException("Argumento ilegal, não pode ser zero");
+        } // Nesse caso o codigo da error e volta para a classe que está sendo executada
         try{
             return a/b;
         }catch (RuntimeException e){
             e.printStackTrace();
         }
-
+        return 0;
 
     }
 }
