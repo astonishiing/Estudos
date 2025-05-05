851227ee2298eeae0d5d6009e1d38f15693e479f
astonishiing <wcardoso4510@gmail.com>
2025-05-05 15:00:00 -0300

    Exemplo de stackOverflow

--- src/javacore/introducao/dominio/Oexception/test/StackOverflowTest01.java
+++ src/javacore/introducao/dominio/Oexception/test/StackOverflowTest01.java
@@ -2,8 +2,11 @@ package javacore.introducao.dominio.Oexception.test;
 
 public class StackOverflowTest01 {
     public static void main(String[] args) {
-
+        recursividade();
     }
+    // Quando a stack estoura, nao tem como resolver
 
-    public static void recursividade
+    public static void recursividade(){
+        recursividade();
+    }
 }
