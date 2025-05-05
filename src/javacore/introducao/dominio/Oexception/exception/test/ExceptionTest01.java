4e92e74ce714782dbb2b80746c93c2d6617665a3
astonishiing <wcardoso4510@gmail.com>
2025-05-05 15:28:54 -0300

    Classe ExceptionTest01

---
+++ src/javacore/introducao/dominio/Oexception/exception/test/ExceptionTest01.java
@@ -0,0 +1,13 @@
+package javacore.introducao.dominio.Oexception.exception.test;
+
+import java.io.File;
+
+public class ExceptionTest01 {
+    public static void main(String[] args) {
+
+    }
+
+    private static void criarNovoArquivo(){
+        File file = new File("arquivo");
+    }
+}
