ab87b78efac7b62dd2def3580098a62997fcad2f
astonishiing <wcardoso4510@gmail.com>
2025-05-05 16:00:49 -0300

    Exemplo de IOException

--- src/javacore/introducao/dominio/Oexception/exception/test/ExceptionTest01.java
+++ src/javacore/introducao/dominio/Oexception/exception/test/ExceptionTest01.java
@@ -1,14 +1,21 @@
 package javacore.introducao.dominio.Oexception.exception.test;
 
 import java.io.File;
+import java.io.IOException;
 
 public class ExceptionTest01 {
     public static void main(String[] args) {
-
+        criarNovoArquivo();
     }
 
-    private static void criarNovoArquivo(){
+    private static void criarNovoArquivo() {
         File file = new File("arquivo\\test.txt");
-        file.createNewFile(); //Nesse exemplo temos um error de compilação, porque a exception não esta sendo tratada
+        try {
+            boolean isCriado = file.createNewFile(); //Nesse exemplo temos um error de compilação, porque a exception não esta sendo tratada
+            System.out.println("Arquivos criado: " + isCriado);
+        } catch (IOException e) { // a classe (Exception) ela é generica, as boas práticas nao recomendam utilizar ela
+            e.printStackTrace();
+        }
+
     }
 }
