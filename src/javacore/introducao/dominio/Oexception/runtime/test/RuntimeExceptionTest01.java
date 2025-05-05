8a7df3cd3b337de14f25ff8d76aaff868b89ee1c
astonishiing <wcardoso4510@gmail.com>
2025-05-05 15:09:41 -0300

    Comments

--- src/javacore/introducao/dominio/Oexception/runtime/test/RuntimeExceptionTest01.java
+++ src/javacore/introducao/dominio/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -2,4 +2,5 @@ package javacore.introducao.dominio.Oexception.runtime.test;
 
 public class RuntimeExceptionTest01 {
     // Checked e uncheked
+    // Exception Checked são exceptions que são filhas da classe exception diretamente, se nao forem tratadas. Eles causarão error em tempo de compilação
 }
