ff7474a2edcabc98142f480e6720a990a833868a
astonishiing <wcardoso4510@gmail.com>
2025-05-05 14:57:39 -0300

    Criacao da class StackOverflow

---
+++ src/javacore/introducao/dominio/Oexception/test/StackOverflowTest01.java
@@ -0,0 +1,9 @@
+package javacore.introducao.dominio.Oexception.test;
+
+public class StackOverflowTest01 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void recursividade
+}
