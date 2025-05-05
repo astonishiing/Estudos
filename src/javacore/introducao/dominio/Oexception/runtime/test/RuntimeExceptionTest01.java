3c2ac90a5584b11a0e90222ad086bfc54adfba34
astonishiing <wcardoso4510@gmail.com>
2025-05-05 15:16:35 -0300

    Exemplo de RunTimeException

--- src/javacore/introducao/dominio/Oexception/runtime/test/RuntimeExceptionTest01.java
+++ src/javacore/introducao/dominio/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -1,6 +1,10 @@
 package javacore.introducao.dominio.Oexception.runtime.test;
 
 public class RuntimeExceptionTest01 {
+    public static void main(String[] args) {
+        Object object = null; // se nós nao dermos um objeto para a variável de referência e tentar acessa-la, teremos um nullPointException
+        System.out.println(object.toString());
+    }
     // Checked e uncheked
     // Exception Checked são exceptions que são filhas da classe exception diretamente, se nao forem tratadas. Eles causarão error em tempo de compilação
 }
