9207e27140b17ec6e33ba530ca565251d57e2a2f
astonishiing <wcardoso4510@gmail.com>
2025-05-07 20:58:09 -0300

    Construtores

--- src/javacore/introducao/dominio/Oexception/exception/domain/LoginInvalidoException.java
+++ src/javacore/introducao/dominio/Oexception/exception/domain/LoginInvalidoException.java
@@ -1,5 +1,11 @@
 package javacore.introducao.dominio.Oexception.exception.domain;
 
-public class LoginInvalidoException {
+public class LoginInvalidoException extends Exception{
+    public LoginInvalidoException() {
+        super("Login Invalido");
+    }
 
+    public LoginInvalidoException(String message) {
+        super(message);
+    }
 }
