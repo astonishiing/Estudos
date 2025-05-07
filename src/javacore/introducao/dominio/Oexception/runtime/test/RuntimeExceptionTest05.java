212d30cc5844265fcef949de2da8cfa1cd77f1f6
astonishiing <wcardoso4510@gmail.com>
2025-05-07 15:50:54 -0300

    RuntimeExceptionTest05

---
+++ src/javacore/introducao/dominio/Oexception/runtime/test/RuntimeExceptionTest05.java
@@ -0,0 +1,30 @@
+package javacore.introducao.dominio.Oexception.runtime.test;
+
+import java.io.FileNotFoundException;
+import java.sql.SQLException;
+
+public class RuntimeExceptionTest05 {
+    public static void main(String[] args) {
+        try {
+            throw new RuntimeException();
+        } catch (ArrayIndexOutOfBoundsException  e) {
+            System.out.println("Dentro do ArrayIndex");
+            e.printStackTrace();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOut");
+        } catch (ArithmeticException e) {
+            System.out.println("Dentro do Aritmentic");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro do RuntimeException");
+        }
+        try{
+            talvezLanceException();
+        } catch (SQLException e){
+
+        }
+    }
+
+    private static void talvezLanceException() throws SQLException, FileNotFoundException{
+
+    }
+}
