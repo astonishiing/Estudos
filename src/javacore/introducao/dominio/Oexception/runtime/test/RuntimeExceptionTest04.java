08e600b42b4a26e480e5aa19c314588a07115ba1
astonishiing <wcardoso4510@gmail.com>
2025-05-07 15:27:19 -0300

    RuntimeException04

--- src/javacore/introducao/dominio/Oexception/runtime/test/RuntimeExceptionTest04.java
+++ src/javacore/introducao/dominio/Oexception/runtime/test/RuntimeExceptionTest04.java
@@ -1,4 +1,17 @@
 package javacore.introducao.dominio.Oexception.runtime.test;
 
 public class RuntimeExceptionTest04 {
+    public static void main(String[] args) {
+        try {
+
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Dentro do ArrayIndex");
+            e.printStackTrace();
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Dentro do IndexOut");
+        }catch (ArithmeticException e){
+            System.out.println("Dentro do Aritmentic");
+        }
+    }
 }
+
