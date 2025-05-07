9e3ded434d04e8f7c75077b721cf56e9d6522f00
astonishiing <wcardoso4510@gmail.com>
2025-05-07 16:04:56 -0300

    lerArquivo2

--- src/javacore/introducao/dominio/Oexception/exception/test/TryWithResourcesTest01.java
+++ src/javacore/introducao/dominio/Oexception/exception/test/TryWithResourcesTest01.java
@@ -1,9 +1,28 @@
 package javacore.introducao.dominio.Oexception.exception.test;
 
+import java.io.*;
+
 public class TryWithResourcesTest01 {
     public static void main(String[] args) {
 
     }
 
-    public static void
+    public static void lerArquivo2() {
+        Reader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("text.txt"));
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
 }
