65d52cbf5a6310b0b22c3437ec4cf9f4519593c3
astonishiing <wcardoso4510@gmail.com>
2025-05-07 21:22:26 -0300

    Try and catch

--- src/javacore/introducao/dominio/Oexception/exception/test/SobrescritaComExceptionTest01.java
+++ src/javacore/introducao/dominio/Oexception/exception/test/SobrescritaComExceptionTest01.java
@@ -1,14 +1,28 @@
 package javacore.introducao.dominio.Oexception.exception.test;
 
 import javacore.introducao.dominio.Oexception.exception.domain.Funcionario;
+import javacore.introducao.dominio.Oexception.exception.domain.LoginInvalidoException;
 import javacore.introducao.dominio.Oexception.exception.domain.Pessoa;
+
+import java.io.FileNotFoundException;
 
 public class SobrescritaComExceptionTest01 {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
         Funcionario funcionario = new Funcionario();
+        try {
+            pessoa.salvar();
+        } catch (LoginInvalidoException e) {
+            throw new RuntimeException(e);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
-        funcionario.salvar();
+        try {
+            funcionario.salvar();
+        } catch (LoginInvalidoException | FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
