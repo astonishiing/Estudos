c997eec3066862b7bdf1a2e9e3cc2ece5b969e6f
astonishiing <wcardoso4510@gmail.com>
2025-05-07 21:23:09 -0300

    Pessoa

--- src/javacore/introducao/dominio/Oexception/exception/domain/Pessoa.java
+++ src/javacore/introducao/dominio/Oexception/exception/domain/Pessoa.java
@@ -1,4 +1,9 @@
 package javacore.introducao.dominio.Oexception.exception.domain;
 
+import java.io.FileNotFoundException;
+
 public class Pessoa {
+    public void salvar() throws LoginInvalidoException, FileNotFoundException{
+        System.out.println("Salvando pessoas");
+    }
 }
