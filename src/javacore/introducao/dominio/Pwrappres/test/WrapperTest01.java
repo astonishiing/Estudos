02616e6b4dfac8b3b769db33caeef571a427ac05
astonishiing <wcardoso4510@gmail.com>
2025-05-09 14:25:25 -0300

    Atributos

--- src/javacore/introducao/dominio/Pwrappres/test/WrapperTest01.java
+++ src/javacore/introducao/dominio/Pwrappres/test/WrapperTest01.java
@@ -2,6 +2,15 @@ package javacore.introducao.dominio.Pwrappres.test;
 
 public class WrapperTest01 {
     public static void main(String[] args) {
-        byte byte
+        // Todas as classes elas são filhas de objeto e os tipos primitivos são apenas tipos primitivos, que são representadas por bits
+        byte byter = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10;
+        char charP = 'W';
+        boolean booleanP = false;
+
     }
 }
