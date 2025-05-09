05c71b9c05b23ad3db3916451adf54839eb55f25
astonishiing <wcardoso4510@gmail.com>
2025-05-09 15:54:18 -0300

    StringTest01 Pool de String

---
+++ src/javacore/introducao/dominio/Qstring/test/StringTest01.java
@@ -0,0 +1,20 @@
+package javacore.introducao.dominio.Qstring.test;
+
+public class StringTest01 {
+    public static void main(String[] args) {
+        // String no java são imutáveis
+        String nome = "Willian"; // String constant pool
+        String nome2 = "Willian";
+        nome = nome.concat(" Suane");
+        System.out.println(nome);
+        // Por causa da imutabilidade das String a não ser que façamos uma nova associação, não podemos trocar o valor que existe
+
+        System.out.println(nome == nome2); // comparar a referência de um objeto
+        String nome3 = new String("Willian");
+        System.out.println(nome2 == nome3);
+        System.out.println(nome2 == nome3.intern());
+        // Então os dois valores então fazendo referência a um mesmo objeto em memória
+        // String é imutável, não é possivel alterar o valor
+
+    }
+}
