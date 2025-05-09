76e8f34b9d1d3660cc6cc67df033d5d2cca65ece
astonishiing <wcardoso4510@gmail.com>
2025-05-09 14:35:21 -0300

    Wrappers

--- src/javacore/introducao/dominio/Pwrappres/test/WrapperTest01.java
+++ src/javacore/introducao/dominio/Pwrappres/test/WrapperTest01.java
@@ -3,16 +3,18 @@ package javacore.introducao.dominio.Pwrappres.test;
 public class WrapperTest01 {
     public static void main(String[] args) {
         // Todas as classes elas são filhas de objeto e os tipos primitivos são apenas tipos primitivos, que são representadas por bits
+        // No exemplo abaixo trabalhamos com bits
         byte byter = 1;
         short shortP = 1;
         int intP = 1;
-        long longP = 10L;
+        long longP = 10L; // se tirarmos o L, ele se torna inteiro,
+        // 1 inteiro é menor que um long, entao todos os inteiros cabem dentro de um long
         float floatP = 10F;
         double doubleP = 10;
         char charP = 'W';
         boolean booleanP = false;
         // Classe wrapper: é escapsular o tipo primitov e transformar em
-
+        // Agora estamos trabalhando com objetos, então a regra de polimofismo é aplicada
         Byte byterW = 1;
         Short shortW = 1;
         Integer intW = 1;
