27ec3bf06bca561771fea4157670e06de9c7671b
astonishiing <wcardoso4510@gmail.com>
2025-06-24 15:08:46 -0300

    CalendarTest

--- src/javacore/introducao/dominio/Rdates/test/CalendarTest.java
+++ src/javacore/introducao/dominio/Rdates/test/CalendarTest.java
@@ -1,4 +1,21 @@
 package javacore.introducao.dominio.Rdates.test;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class CalendarTest {
+    public static void main(String[] args) {
+        Calendar c = Calendar.getInstance();
+        if(c.getFirstDayOfWeek() == Calendar.SUNDAY){
+            System.out.println("Domigo, é o primeiro dia da semana");
+        }
+        System.out.println(c.get(Calendar.DAY_OF_WEEK));
+        System.out.println(c.get(Calendar.DAY_OF_MONTH));
+        System.out.println(c.get(Calendar.DAY_OF_YEAR));
+        System.out.println(c.get(Calendar.DAY_OF_WEEK_IN_MONTH));
+
+        Date date = c.getTime();
+        System.out.println(date);
+    }
+
 }
