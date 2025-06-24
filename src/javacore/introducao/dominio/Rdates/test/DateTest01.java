8991c8e8ef3f2ba06605a3eae9d9d7485576a9f2
astonishiing <wcardoso4510@gmail.com>
2025-06-24 14:37:24 -0300

    DateTest

--- src/javacore/introducao/dominio/Rdates/test/DateTest01.java
+++ src/javacore/introducao/dominio/Rdates/test/DateTest01.java
@@ -1,5 +1,13 @@
 package javacore.introducao.dominio.Rdates.test;
 
+import java.util.Date;
+
 public class DateTest01 {
+    public static void main(String[] args) {
+        Date date = new Date(1750786599260L); //Trabalha com milisegundos (Long)
+        System.out.println(date.getTime());
+    }
+
+
 
 }
