c0bf14bfa695e425ef530861b5965197405c279d
astonishiing <wcardoso4510@gmail.com>
2025-06-24 14:28:10 -0300

    StringBuilder

--- src/javacore/introducao/dominio/Qstring/test/StringBuilderTest01.java
+++ src/javacore/introducao/dominio/Qstring/test/StringBuilderTest01.java
@@ -3,7 +3,12 @@ package javacore.introducao.dominio.Qstring.test;
 public class StringBuilderTest01 {
     public static void main(String[] args) {
         String nome = "Willian";
-        nome.concat(" TestBuffer");
+        nome = nome.concat(" TestBuffer");
         System.out.println(nome);
+        StringBuilder sb = new StringBuilder("Willian"); //Caso criamos e não passamos nenhum valor, ela é iniciado com 16
+        sb.append(" TestBuilder").append(" 1234");
+        sb.reverse();
+        System.out.println(sb);
+
     }
 }
