1a44038364c66e475eb9a1cef9913f9fd1eabaec
astonishiing <wcardoso4510@gmail.com>
2025-06-24 14:19:15 -0300

    StringBuilder

---
+++ src/javacore/introducao/dominio/Qstring/test/StringBuilderTest01.java
@@ -0,0 +1,9 @@
+package javacore.introducao.dominio.Qstring.test;
+
+public class StringBuilderTest01 {
+    public static void main(String[] args) {
+        String nome = "Willian";
+        nome.concat(" TestBuffer");
+        System.out.println(nome);
+    }
+}
