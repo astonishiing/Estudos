d7abee3e6632aa2283301196d9207c8fab0d3249
astonishiing <wcardoso4510@gmail.com>
2025-06-25 17:58:03 -0300

    LocalTimeTest01

---
+++ src/javacore/introducao/dominio/Rdates/test/LocalTimeTest01.java
@@ -0,0 +1,10 @@
+package javacore.introducao.dominio.Rdates.test;
+
+import java.time.LocalTime;
+
+public class LocalTimeTest01 {
+    public static void main(String[] args) {
+        LocalTime time = LocalTime.of(23, 32, 12);
+        System.out.println(time);
+    }
+}
