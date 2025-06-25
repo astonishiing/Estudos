73c0a91a04b5245c2fc368fc78084b40380383af
astonishiing <wcardoso4510@gmail.com>
2025-06-25 09:56:12 -0300

    NumberFormatTest02

--- src/javacore/introducao/dominio/Sformatacao/test/NumberFormatTest02.java
+++ src/javacore/introducao/dominio/Sformatacao/test/NumberFormatTest02.java
@@ -1,4 +1,36 @@
 package javacore.introducao.dominio.Sformatacao.test;
 
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+
 public class NumberFormatTest02 {
+    public static void main(String[] args) {
+        Locale localeDefault = Locale.getDefault();
+        Locale localePT = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+        NumberFormat[] nfa = new NumberFormat[4];
+
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localePT);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[3] = NumberFormat.getCurrencyInstance(localeIT);
+
+        double valor = 10_000.230;
+
+        for (NumberFormat numberFormat : nfa) {
+            System.out.println(numberFormat.getMaximumFractionDigits());
+            //numberFormat.setMaximumFractionDigits(2);
+            System.out.println(numberFormat.format(valor));
+        }
+        String valorString = "￥10,000";
+
+        try {
+            System.out.println(nfa[2].parse(valorString));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
