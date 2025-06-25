4541c387ccce86ac1bb0dea421db972a345214ee
astonishiing <wcardoso4510@gmail.com>
2025-06-25 09:43:10 -0300

    NumberFormatTest01

--- src/javacore/introducao/dominio/Sformatacao/test/NumberFormatTest01.java
+++ src/javacore/introducao/dominio/Sformatacao/test/NumberFormatTest01.java
@@ -1,4 +1,26 @@
 package javacore.introducao.dominio.Sformatacao.test;
 
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class NumberFormatTest01 {
+    public static void main(String[] args) {
+        Locale localeDefault = Locale.getDefault();
+        Locale localePT = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+        NumberFormat[] nfa = new NumberFormat[4];
+
+        nfa[0] = NumberFormat.getInstance();
+        nfa[1] = NumberFormat.getInstance(localePT);
+        nfa[2] = NumberFormat.getInstance(localeJP);
+        nfa[3] = NumberFormat.getInstance(localeIT);
+
+        double valor = 10_000.230;
+
+        for (NumberFormat numberFormat : nfa) {
+            System.out.println(numberFormat.format(valor));
+        }
+
+    }
 }
