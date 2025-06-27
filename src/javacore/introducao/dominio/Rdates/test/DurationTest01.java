bdb77522c6f63976210b50903c5bda751a91602b
astonishiing <wcardoso4510@gmail.com>
2025-06-27 14:03:15 -0300

    DurationTest01

--- src/javacore/introducao/dominio/Rdates/test/DurationTest01.java
+++ src/javacore/introducao/dominio/Rdates/test/DurationTest01.java
@@ -3,19 +3,31 @@ package javacore.introducao.dominio.Rdates.test;
 import javacore.introducao.dominio.Gassociacao.dominio.Local;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 
 public class DurationTest01 {
     public static void main(String[] args) {
         LocalDateTime now = LocalDateTime.now();
-        LocalDateTime nowAfterTwoYears = LocalDateTime.now().plusYears(2);
+        LocalDateTime nowAfterTwoYears = LocalDateTime.now().plusYears(2).plusMinutes(8);
         LocalTime timeNow = LocalTime.now();
         LocalTime timeMinus7Hours = LocalTime.now().plusHours(7);
 
 
         //ISO 8601
+        //A duration não aceita LocalDate, por não ter segundos
         Duration d1 = Duration.between(now, nowAfterTwoYears);
+        Duration d2 = Duration.between(timeNow, timeMinus7Hours);
+        Duration d3 = Duration.between(Instant.now(), Instant.now().plusSeconds(1000));
+        Duration d4 = Duration.ofDays(20);
+        Duration d5 = Duration.of(3, ChronoUnit.HOURS);
         System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(d4);
+        System.out.println(d5);
     }
 }
