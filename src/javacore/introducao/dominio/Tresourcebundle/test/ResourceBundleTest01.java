775590a05ed24135d721d38474d786b7f6c2b421
astonishiing <wcardoso4510@gmail.com>
2025-06-29 15:52:21 -0300

    ResourceBudleTest

---
+++ src/javacore/introducao/dominio/Tresourcebundle/test/ResourceBundleTest01.java
@@ -0,0 +1,9 @@
+package javacore.introducao.dominio.Tresourcebundle.test;
+
+import java.util.Locale;
+
+public class ResourceBundleTest01 {
+    public static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+    }
+}
