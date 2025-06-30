c68d30b8c80b13f88f205575355f072b5e73733c
astonishiing <wcardoso4510@gmail.com>
2025-06-29 21:04:46 -0300

    PatternMatcher

---
+++ src/javacore/introducao/dominio/Uregex/test/PatternMatcherTest02.java
@@ -0,0 +1,22 @@
+package javacore.introducao.dominio.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest02 {
+    public static void main(String[] args) {
+        // willian.suane@dev.academy
+        String regex = "ab";
+        String texto = "abaaba";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto:    " + texto);
+        System.out.println("indice: 0123456789 ");
+        System.out.println("regex:   " + regex);
+        System.out.println("Posições encontradas");
+
+        while(matcher.find()){
+            System.out.print(matcher.start() + " " + matcher.group() +"\n");
+        }
+    }
+}
