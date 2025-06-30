5232689d1bf61a542de27427896ed799550fbbdd
astonishiing <wcardoso4510@gmail.com>
2025-06-30 14:18:55 -0300

    PatternMatcher

---
+++ src/javacore/introducao/dominio/Uregex/test/PatternMatcherTest05.java
@@ -0,0 +1,36 @@
+package javacore.introducao.dominio.Uregex.test;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PatternMatcherTest05 {
+    public static void main(String[] args) {
+        // \d = Todos os digitos
+        // \D = Tudo o que não for digito
+        // \s = Espaços em branco \t \n \f \r
+        // \S = Todos os caracteres excluindo os brancos
+        // \w = a-z ou A-Z, todos os dígitos e _
+        // \W = Tudo o que não for iniciado no \w
+        // []
+        // ? Zero ou uma ocorrência
+        // * Zero ou mais
+        // + uma ou mais
+        // {n,m} de n até m
+        // | a(v|c)o ovo | oco
+        // $ Representa o fim de linha
+        // . = 123 133 1@3 1A3
+
+        String regex = "0[xX]([0-9a-fA-F])+(\\s|$)";
+        String texto = "12 0x 0x 0xFFABC 0x10G 0x1";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(texto);
+        System.out.println("texto:    " + texto);
+        System.out.println("indice: 0123456789 ");
+        System.out.println("regex:   " + regex);
+        System.out.println("Posições encontradas");
+
+        while(matcher.find()){
+            System.out.print(matcher.start() + " " + matcher.group() +"\n");
+        }
+    }
+}
