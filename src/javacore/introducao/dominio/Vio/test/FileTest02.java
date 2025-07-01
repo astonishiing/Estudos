0c7b5d33291e2971673aa74500c885a34efd0307
astonishiing <wcardoso4510@gmail.com>
2025-07-01 15:28:06 -0300

    FileTest02

--- src/javacore/introducao/dominio/Vio/test/FileTest02.java
+++ src/javacore/introducao/dominio/Vio/test/FileTest02.java
@@ -7,14 +7,20 @@ public class FileTest02 {
     public static void main(String[] args) throws IOException {
         File fileDiretorio = new File("pasta");
         boolean mkdir = fileDiretorio.mkdir();
-        System.out.println(mkdir);
+        System.out.println("Pasta criada? "+ mkdir);
+
         File fileAquivoDiretorio = new File(fileDiretorio,"arquivo.txt");
         boolean isFileCreated = fileAquivoDiretorio.createNewFile();
-        System.out.println(isFileCreated);
+        System.out.println("arquivo.txt criado?"+ isFileCreated);
 
         File fileRenamed = new File(fileDiretorio,"arquivo_renomeado.txt");
         boolean isRenamed = fileAquivoDiretorio.renameTo(fileRenamed);
-        System.out.println(isRenamed);
+        System.out.println("arquivo.txt renoemado?"+ isRenamed);
+
+        File diretorioRenamed = new File("pasta2");
+        boolean isDirectoryRenamed = fileDiretorio.renameTo(diretorioRenamed);
+        System.out.println("Diretorio pasta renomeado? "+isDirectoryRenamed);
+
 //        File fileArquivoDiretorio = new File("C:\\Users\\x\\IdeaProjects\\Estudos\\pasta\\arquivo.txt");
 //        boolean isFileCreated = fileArquivoDiretorio.createNewFile();
 //        System.out.println(isFileCreated);
