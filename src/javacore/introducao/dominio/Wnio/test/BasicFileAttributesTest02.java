aadecb263a4b48214b4e09cb0081bd6563e4061b
astonishiing <wcardoso4510@gmail.com>
2025-07-02 15:04:19 -0300

    BasicFileAttributesTest02

--- src/javacore/introducao/dominio/Wnio/test/BasicFileAttributesTest02.java
+++ src/javacore/introducao/dominio/Wnio/test/BasicFileAttributesTest02.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributeView;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
 
@@ -11,6 +12,7 @@ public class BasicFileAttributesTest02 {
     public static void main(String[] args) throws IOException {
         Path path = Paths.get("pasta2/novo_path.txt");
         BasicFileAttributes basicFileAttributes = Files.readAttributes(path, BasicFileAttributes.class);
+
         FileTime creationTime = basicFileAttributes.creationTime();
         FileTime lastModifiedTime = basicFileAttributes.lastModifiedTime();
         FileTime lastAccessTime = basicFileAttributes.lastAccessTime();
@@ -18,6 +20,20 @@ public class BasicFileAttributesTest02 {
         System.out.println("creationTime " + creationTime);
         System.out.println("lastModifiedTime " + lastModifiedTime);
         System.out.println("lastAccessTime " + lastAccessTime);
+        System.out.println("-------------");
+        BasicFileAttributeView fileAttributeView = Files.getFileAttributeView(path, BasicFileAttributeView.class);
+        FileTime newCreationTime = FileTime.fromMillis(System.currentTimeMillis());
+
+        fileAttributeView.setTimes(lastModifiedTime,newCreationTime ,creationTime);
+
+        creationTime = fileAttributeView.readAttributes().creationTime();
+        lastModifiedTime = fileAttributeView.readAttributes().lastModifiedTime();
+        lastAccessTime = fileAttributeView.readAttributes().lastAccessTime();
+
+        System.out.println("creationTime " + creationTime);
+        System.out.println("lastModifiedTime " + lastModifiedTime);
+        System.out.println("lastAccessTime " + lastAccessTime);
+
 
     }
 }
