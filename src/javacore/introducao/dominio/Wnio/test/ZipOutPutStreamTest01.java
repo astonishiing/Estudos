98c91eb0383d24054304b06cb5f910a0616c2b92
astonishiing <wcardoso4510@gmail.com>
2025-07-03 15:58:35 -0300

    ZipOutPutStreamTest

--- src/javacore/introducao/dominio/Wnio/test/ZipOutPutStreamTest01.java
+++ src/javacore/introducao/dominio/Wnio/test/ZipOutPutStreamTest01.java
@@ -1,7 +1,33 @@
 package javacore.introducao.dominio.Wnio.test;
 
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 public class ZipOutPutStreamTest01 {
     public static void main(String[] args) {
+        Path arquivoZip = Paths.get("pastaWnio/arquivo.zip");
+        Path arquivoParaZipar = Paths.get("pastaWnio/subpasta1/subsubpasta1");
+        zip(arquivoZip, arquivoParaZipar);
+    }
+    private static void zip(Path arquivoZip, Path arquivoParaZipar){
+        try(ZipOutputStream zipStream = new ZipOutputStream(Files.newOutputStream(arquivoZip));
+            DirectoryStream<Path> directoryStream = Files.newDirectoryStream(arquivoParaZipar)){
 
+            for(Path file : directoryStream) {
+                ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
+                zipStream.putNextEntry(zipEntry);
+                Files.copy(file, zipStream);
+                zipStream.closeEntry();
+                //System.out.println(file.getFileName());
+            }
+            System.out.println("Arquivo criado com sucesso");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
