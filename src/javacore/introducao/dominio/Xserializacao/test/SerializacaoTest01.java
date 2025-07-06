5a34283a7c9b0965fdf51a6393d316248a1498dd
astonishiing <wcardoso4510@gmail.com>
2025-07-06 17:04:06 -0300

    XSerilizacao

--- src/javacore/introducao/dominio/Xserializacao/test/SerializacaoTest01.java
+++ src/javacore/introducao/dominio/Xserializacao/test/SerializacaoTest01.java
@@ -2,9 +2,7 @@ package javacore.introducao.dominio.Xserializacao.test;
 
 import javacore.introducao.dominio.Xserializacao.dominio.Aluno;
 
-import java.io.IOException;
-import java.io.ObjectOutput;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,7 +10,8 @@ import java.nio.file.Paths;
 public class SerializacaoTest01 {
     public static void main(String[] args) {
         Aluno aluno = new Aluno(1L, "WillianSuane", "123412121");
-        serializar(aluno);
+        //serializar(aluno);
+        deserializar();
     }
 
     private static void serializar(Aluno aluno) {
@@ -20,6 +19,16 @@ public class SerializacaoTest01 {
         try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(Files.newOutputStream(path))) {
             objectOutputStream.writeObject(aluno);
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void deserializar() {
+        Path path = Paths.get("pasta/aluno.ser");
+        try (ObjectInputStream ObjectInputStream = new ObjectInputStream(Files.newInputStream(path))) {
+            Aluno aluno = (Aluno) ObjectInputStream.readObject();
+            System.out.println(aluno);
+        } catch (ClassNotFoundException | IOException e) {
             e.printStackTrace();
         }
     }
