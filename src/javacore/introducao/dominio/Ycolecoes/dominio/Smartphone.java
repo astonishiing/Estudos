ddffdf4a91244e90cd4f3da729aff2a1a2db90a3
astonishiing <wcardoso4510@gmail.com>
2025-07-07 10:13:35 -0300

    hashCode

--- src/javacore/introducao/dominio/Ycolecoes/dominio/Smartphone.java
+++ src/javacore/introducao/dominio/Ycolecoes/dominio/Smartphone.java
@@ -8,6 +8,16 @@ public class Smartphone {
         this.serialNumber = serialNumber;
         this.marca = marca;
     }
+
+    //Se x.equals(y) == true, y.hashCode(x) == x.hashCode()
+    // y.hashCode() == x.hashCode() não necessariamente o equals de y.equals(x) tem que ser true
+    // x.equals(y) == false
+    // y.hashCode() != x.hashCode() x,equals(y) deverá ser false.
+
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
+    }
     // Reflexivo: x.equals(x) tem que ser true para tudo que for diferente de null
     // Simétrico: para x e y diferentes de null, se x.equals(y) == true logo, y.equals(x) == true
     // Transitividade: para x,y,z diferentes de null, se x.quals(y) == true, e x.equals(z) == true, logo y.equals(z) == true
