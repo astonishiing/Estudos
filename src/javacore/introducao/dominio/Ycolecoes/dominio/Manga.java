34eebf688fe26397ea18b94c7a66d01d6319c1fa
astonishiing <wcardoso4510@gmail.com>
2025-07-12 14:54:30 -0300

    Manga

---
+++ src/javacore/introducao/dominio/Ycolecoes/dominio/Manga.java
@@ -0,0 +1,64 @@
+package javacore.introducao.dominio.Ycolecoes.dominio;
+
+import java.util.Objects;
+
+public class Manga {
+    private Long id;
+    private String nome;
+    private double preço;
+
+    public Manga(Long id, String nome, double preço) {
+        Objects.requireNonNull(id);
+        Objects.requireNonNull(nome);
+        Objects.requireNonNull(preço);
+
+        this.id = id;
+        this.nome = nome;
+        this.preço = preço;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Manga manga = (Manga) o;
+        return Double.compare(preço, manga.preço) == 0 && Objects.equals(id, manga.id) && Objects.equals(nome, manga.nome);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nome, preço);
+    }
+
+    @Override
+    public String toString() {
+        return "Manga{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", preço=" + preço +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreço() {
+        return preço;
+    }
+
+    public void setPreço(double preço) {
+        this.preço = preço;
+    }
+}
