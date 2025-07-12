83999e66bde228d0ad8951b9e88c48c9a3c8afe8
astonishiing <wcardoso4510@gmail.com>
2025-07-12 15:10:05 -0300

    MangaSort

--- src/javacore/introducao/dominio/Ycolecoes/test/MangaSortTest01.java
+++ src/javacore/introducao/dominio/Ycolecoes/test/MangaSortTest01.java
@@ -1,15 +1,29 @@
 package javacore.introducao.dominio.Ycolecoes.test;
 
+import javacore.introducao.dominio.Ycolecoes.dominio.Manga;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MangaSortTest01 {
     public static void main(String[] args) {
-        List<String> mangas = new ArrayList<>(6);
-        mangas.add("Helsing Ultimate");
-        mangas.add("Berserk");
-        mangas.add("Pokemin");
-        mangas.add("Attack on Titan");
-        mangas.add("Dragon ball Z");
+        List<Manga> mangas = new ArrayList<>(6);
+        mangas.add(new Manga(5L,"Helsing Ultimate", 19.9));
+        mangas.add(new Manga(1L,"Berserk", 9.5));
+        mangas.add(new Manga(4L,"Pokemin", 3.2));
+        mangas.add(new Manga(3L,"Attack on Titan",11.20));
+        mangas.add(new Manga(2L,"Dragon ball Z", 2.99));
+
+        for (Manga manga : mangas){
+            System.out.println(manga);
+        }
+
+        Collections.sort(mangas);
+        System.out.println("--------------");
+
+        for (Manga manga : mangas){
+            System.out.println(manga);
+        }
     }
 }
