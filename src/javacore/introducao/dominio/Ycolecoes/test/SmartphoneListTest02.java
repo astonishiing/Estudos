0baf7b7d796fd014f3c7eba4a0f4a29fe8efd494
astonishiing <wcardoso4510@gmail.com>
2025-07-12 14:03:13 -0300

    SmartList

---
+++ src/javacore/introducao/dominio/Ycolecoes/test/SmartphoneListTest02.java
@@ -0,0 +1,12 @@
+package javacore.introducao.dominio.Ycolecoes.test;
+
+import javacore.introducao.dominio.Ycolecoes.dominio.Smartphone;
+
+public class SmartphoneListTest02 {
+    public static void main(String[] args) {
+        Smartphone s1 = new Smartphone("1ABC1", "iPhone");
+        Smartphone s2 = new Smartphone("2222","Pixel");
+        Smartphone s3 = new Smartphone("3333", "Samgung");
+
+    }
+}
