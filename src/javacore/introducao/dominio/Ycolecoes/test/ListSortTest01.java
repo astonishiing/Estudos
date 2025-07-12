5672e6583f6784fb0843485c8c334380a36194a7
astonishiing <wcardoso4510@gmail.com>
2025-07-12 14:41:59 -0300

    ListSort[

--- src/javacore/introducao/dominio/Ycolecoes/test/ListSortTest01.java
+++ src/javacore/introducao/dominio/Ycolecoes/test/ListSortTest01.java
@@ -13,10 +13,23 @@ public class ListSortTest01 {
         mangas.add("Helsing Ultimate");
         mangas.add("Pokemon");
         mangas.add("Dragon ball z");
-        Collections.sort(mangas);
+
+        Collections.sort(mangas); //Ordernar por ordem alfabetica
+
+        List<Double> dinheiros = new ArrayList<>();
+        dinheiros.add(100.21);
+        dinheiros.add(23.98);
+        dinheiros.add(21.21);
+        dinheiros.add(98.10);
+
+
 
         for(String manga : mangas){
             System.out.println(manga);
         }
+        System.out.println(dinheiros);
+
+        Collections.sort(dinheiros);
+        System.out.println(dinheiros);
     }
 }
