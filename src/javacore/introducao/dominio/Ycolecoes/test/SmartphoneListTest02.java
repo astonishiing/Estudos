6d511a6f3511847e337d92db3ae32c481b604909
astonishiing <wcardoso4510@gmail.com>
2025-07-12 14:19:25 -0300

    SmartphoneList

--- src/javacore/introducao/dominio/Ycolecoes/test/SmartphoneListTest02.java
+++ src/javacore/introducao/dominio/Ycolecoes/test/SmartphoneListTest02.java
@@ -2,11 +2,30 @@ package javacore.introducao.dominio.Ycolecoes.test;
 
 import javacore.introducao.dominio.Ycolecoes.dominio.Smartphone;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SmartphoneListTest02 {
     public static void main(String[] args) {
         Smartphone s1 = new Smartphone("1ABC1", "iPhone");
         Smartphone s2 = new Smartphone("2222","Pixel");
         Smartphone s3 = new Smartphone("3333", "Samgung");
+        List<Smartphone> smartphones = new ArrayList<>(6);
 
+        smartphones.add(s1);
+        smartphones.add(s2);
+        smartphones.add(s3);
+
+        for(Smartphone smartphone : smartphones){
+            System.out.println(smartphone);
+        }
+
+        Smartphone s4 = new Smartphone("2222","Pixel");
+        System.out.println(s4.equals(s2));
+        System.out.println(smartphones.contains(s4));
+
+        int indexSmartphone4 = smartphones.indexOf(s4);
+        System.out.println(indexSmartphone4);
+        System.out.println(smartphones.get(indexSmartphone4)); //Objeto que está dentro da lista
     }
 }
