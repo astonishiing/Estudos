0d900d2aeb911ad4bfbd25419ac5d7357a8c34e5
astonishiing <wcardoso4510@gmail.com>
2025-07-12 14:58:40 -0300

    MangaSort

---
+++ src/javacore/introducao/dominio/Ycolecoes/test/MangaSortTest01.java
@@ -0,0 +1,15 @@
+package javacore.introducao.dominio.Ycolecoes.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MangaSortTest01 {
+    public static void main(String[] args) {
+        List<String> mangas = new ArrayList<>(6);
+        mangas.add("Helsing Ultimate");
+        mangas.add("Berserk");
+        mangas.add("Pokemin");
+        mangas.add("Attack on Titan");
+        mangas.add("Dragon ball Z");
+    }
+}
