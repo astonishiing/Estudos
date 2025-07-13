c51fba96036d44130fb04288f82d3a5d769569f8
astonishiing <wcardoso4510@gmail.com>
2025-07-13 14:13:14 -0300

    ListArrayConversaoTest01

--- src/javacore/introducao/dominio/Ycolecoes/test/ListaArrayConversaoTest01.java
+++ src/javacore/introducao/dominio/Ycolecoes/test/ListaArrayConversaoTest01.java
@@ -10,7 +10,25 @@ public class ListaArrayConversaoTest01 {
         numeros.add(1);
         numeros.add(2);
         numeros.add(3);
-        Integer[] intergersArray = numeros.toArray(new Integer[0]);
-        System.out.println(Arrays.toString(intergersArray));
+        Integer[] listToArray = numeros.toArray(new Integer[0]);
+        System.out.println(Arrays.toString(listToArray));
+        System.out.println("------------");
+        Integer[] numerosArray = new Integer[3];
+        numerosArray[0] = 1;
+        numerosArray[1] = 2;
+        numerosArray[2] = 3;
+
+        List<Integer> arrayToList = Arrays.asList(numerosArray);
+        arrayToList.set(0, 12);
+        System.out.println(Arrays.toString(numerosArray));
+        System.out.println(arrayToList);
+
+        System.out.println("--------------");
+
+        List<Integer> numerosList = new ArrayList<>(Arrays.asList(numerosArray));
+        numerosList.add(15);
+        System.out.println(numerosList);
+
+        Arrays.asList(1,2,3,4,5);
     }
 }
