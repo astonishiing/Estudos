b5218b2d990231d16e3e40c96bf63ae6aa99ae70
astonishiing <wcardoso4510@gmail.com>
2025-07-13 13:44:31 -0300

    BinarySearch

---
+++ src/javacore/introducao/dominio/Ycolecoes/test/BinarySearchTest01.java
@@ -0,0 +1,14 @@
+package javacore.introducao.dominio.Ycolecoes.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinarySearchTest01 {
+    public static void main(String[] args) {
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(2);
+        numeros.add(0);
+        numeros.add(4);
+        numeros.add(3);
+    }
+}
