4c4bb00e551f940ff51659c3b0a89762fd047bff
astonishiing <wcardoso4510@gmail.com>
2025-07-13 13:50:10 -0300

    BinarySearch

--- src/javacore/introducao/dominio/Ycolecoes/test/BinarySearchTest01.java
+++ src/javacore/introducao/dominio/Ycolecoes/test/BinarySearchTest01.java
@@ -1,6 +1,7 @@
 package javacore.introducao.dominio.Ycolecoes.test;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class BinarySearchTest01 {
@@ -10,5 +11,11 @@ public class BinarySearchTest01 {
         numeros.add(0);
         numeros.add(4);
         numeros.add(3);
+        //(-(ponto de inserçõa) - 1)
+        // index 0,1,2,3
+        // value 0,2,3,4
+        Collections.sort(numeros);
+        System.out.println(Collections.binarySearch(numeros,2));
+
     }
 }
