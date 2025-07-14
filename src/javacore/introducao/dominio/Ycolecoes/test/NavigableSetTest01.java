c44443f31f3a636f9bd5f8444aba614f0349eeb3
astonishiing <wcardoso4510@gmail.com>
2025-07-14 13:47:50 -0300

    NavigableSet

---
+++ src/javacore/introducao/dominio/Ycolecoes/test/NavigableSetTest01.java
@@ -0,0 +1,13 @@
+package javacore.introducao.dominio.Ycolecoes.test;
+
+import javacore.introducao.dominio.Ycolecoes.dominio.Smartphone;
+
+import java.util.NavigableSet;
+import java.util.TreeSet;
+
+public class NavigableSetTest01 {
+    public static void main(String[] args) {
+        NavigableSet<Smartphone> set = new TreeSet<>();
+
+    }
+}
