b59056b6c287eb914afec2a72294d89c7f21d9be
astonishiing <wcardoso4510@gmail.com>
2025-07-14 13:58:07 -0300

    NavigableSetTest

--- src/javacore/introducao/dominio/Ycolecoes/test/NavigableSetTest01.java
+++ src/javacore/introducao/dominio/Ycolecoes/test/NavigableSetTest01.java
@@ -1,13 +1,34 @@
 package javacore.introducao.dominio.Ycolecoes.test;
 
+import javacore.introducao.dominio.Ycolecoes.dominio.Manga;
 import javacore.introducao.dominio.Ycolecoes.dominio.Smartphone;
 
-import java.util.NavigableSet;
-import java.util.TreeSet;
+import java.util.*;
+
+class SmartphoneMarcaComparator implements Comparator<Smartphone>{
+
+    @Override
+    public int compare(Smartphone o1, Smartphone o2) {
+        return o1.getMarca().compareTo(o2.getMarca());
+    }
+}
 
 public class NavigableSetTest01 {
     public static void main(String[] args) {
-        NavigableSet<Smartphone> set = new TreeSet<>();
+        NavigableSet<Smartphone> set = new TreeSet<>(new SmartphoneMarcaComparator());
+        Smartphone smartphone = new Smartphone("123", "Nokia");
+        set.add(smartphone);
+        NavigableSet<Manga> manga = new TreeSet<>();
+
+        List<Manga> mangas = new ArrayList<>(6);
+        mangas.add(new Manga(5L,"Helsing Ultimate", 19.9));
+        mangas.add(new Manga(1L,"Berserk", 9.5));
+        mangas.add(new Manga(4L,"Pokemin", 3.2));
+        mangas.add(new Manga(3L,"Attack on Titan",11.20));
+        mangas.add(new Manga(2L,"Dragon ball Z", 2.99));
+        for (Manga mangas1 : mangas) {
+            System.out.println(mangas1);
+        }
 
     }
 }
