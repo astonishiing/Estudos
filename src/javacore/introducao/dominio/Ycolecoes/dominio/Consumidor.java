2fc62b4992cd946b997437a591341d969e0b0f1b
astonishiing <wcardoso4510@gmail.com>
2025-07-16 21:13:21 -0300

    MapTest02

---
+++ src/javacore/introducao/dominio/Ycolecoes/dominio/Consumidor.java
@@ -0,0 +1,4 @@
+package javacore.introducao.dominio.Ycolecoes.dominio;
+
+public class Consumidor {
+}
---
+++ src/javacore/introducao/dominio/Ycolecoes/test/MapTest02.java
@@ -0,0 +1,7 @@
+package javacore.introducao.dominio.Ycolecoes.test;
+
+public class MapTest02 {
+    public static void main(String[] args) {
+
+    }
+}
