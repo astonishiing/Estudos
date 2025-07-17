885ec91cc4058ae4b8fac1fff5fed98d6befc3e5
astonishiing <wcardoso4510@gmail.com>
2025-07-16 21:26:13 -0300

    MapTest03

---
+++ src/javacore/introducao/dominio/Ycolecoes/test/MapTest03.java
@@ -0,0 +1,29 @@
+package javacore.introducao.dominio.Ycolecoes.test;
+
+import javacore.introducao.dominio.Ycolecoes.dominio.Consumidor;
+import javacore.introducao.dominio.Ycolecoes.dominio.Manga;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTest03 {
+    public static void main(String[] args) {
+        Consumidor consumidor1 = new Consumidor("Willian");
+        Consumidor consumidor2 = new Consumidor("Dev");
+
+        Manga manga1 = new Manga(5L, "Helsing Ultimate", 19.9);
+        Manga manga2 = new Manga(1L,"Berserk", 9.5);
+        Manga manga3 = new Manga(4L,"Pokemin", 3.2);
+        Manga manga4 = new Manga(3L,"Attack on Titan",11.20);
+        Manga manga5 = new Manga(2L,"Dragon ball Z", 2.99);
+
+        Map<Consumidor, Manga> consumidorMangaMap = new HashMap<>();
+        consumidorMangaMap.put(consumidor1, manga1);
+        consumidorMangaMap.put(consumidor2, manga4);
+
+        for(Map.Entry<Consumidor, Manga> entry : consumidorMangaMap.entrySet()){
+            System.out.println(entry.getKey().getNome() + " - " + entry.getValue().getNome());
+        }
+
+    }
+}
