d899f6202f8837dfcdfc18c1f1589442d581a640
astonishiing <wcardoso4510@gmail.com>
2025-07-19 09:41:44 -0300

    Generics

--- src/javacore/introducao/dominio/Zgenerics/test/GenericsTest01.java
+++ src/javacore/introducao/dominio/Zgenerics/test/GenericsTest01.java
@@ -1,5 +1,28 @@
 package javacore.introducao.dominio.Zgenerics.test;
 
+import javacore.introducao.dominio.Ycolecoes.dominio.Consumidor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class GenericsTest01 {
+    public static void main(String[] args) {
+        //Type erasure
+        List<String> list = new ArrayList<>(); // Em tempo de compilação
+        list.add("Midoria");
+
+        for(String o : list){
+            System.out.println(o);
+        }
+        add(list, new Consumidor("Midoria"));
+
+        for (Object o : list){
+            System.out.println(o);
+        }
+    }
+    private static void add(List<String> lista, Consumidor consumidor){
+        lista.add(String.valueOf(consumidor));
+    }
 
 }
