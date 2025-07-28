3cf17477474b040cef538ad861924a076ff4a67c
astonishiing <wcardoso4510@gmail.com>
2025-07-28 15:23:34 -0300

    Criacao de classes

---
+++ src/javacore/introducao/dominio/Zgenerics/domain/Carro.java
@@ -0,0 +1,16 @@
+package javacore.introducao.dominio.Zgenerics.domain;
+
+public class Carro {
+    private String nome;
+
+    public Carro(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Carro{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
+}
