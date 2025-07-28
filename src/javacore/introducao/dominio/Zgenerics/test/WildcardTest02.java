bc47dbd65396c8466f82cca7aa5c72f55a6a863e
astonishiing <wcardoso4510@gmail.com>
2025-07-28 15:04:27 -0300

    WildCards

---
+++ src/javacore/introducao/dominio/Zgenerics/test/WildcardTest02.java
@@ -0,0 +1,16 @@
+package javacore.introducao.dominio.Zgenerics.test;
+
+import java.util.List;
+
+
+public class WildcardTest02 {
+    public static void main(String[] args) {
+        List<Cachorro> cachorros = List.of(new Cachorro());
+        List<>
+    }
+    private static void printConsulta(Animal[] animals){
+        for (Animal animal : animals) {
+            animal.consulta();
+        }
+    }
+}
