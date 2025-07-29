3bc82f8f38be3abc172ec1d39339c02675aa3948
test <test@gmail.com>
2025-07-29 13:34:56 -0300

    OuterClasses

--- src/javacore/introducao/dominio/ZZAclassesinternas/test/OuterClassesTest02.java
+++ src/javacore/introducao/dominio/ZZAclassesinternas/test/OuterClassesTest02.java
@@ -3,10 +3,13 @@ package javacore.introducao.dominio.ZZAclassesinternas.test;
 public class OuterClassesTest02 {
     private String name = "Midorya";
 
-    void print(){
+    void print(final String parametro){
+        final String lastName = "Izuku";
+
         class LocalClass {
             public void printLocal(){
-                System.out.println(name);
+                System.out.println(parametro);
+                System.out.println(name + " " + lastName);
             }
         }
 
@@ -16,6 +19,6 @@ public class OuterClassesTest02 {
 
     public static void main(String[] args) {
         OuterClassesTest02 outer = new OuterClassesTest02();
-        outer.print();
+        outer.print("");
     }
 }
