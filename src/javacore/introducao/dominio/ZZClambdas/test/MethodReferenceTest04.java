d6ef55836df9b1eade1e53f8faa9f47795eb7336
astonishiing <wcardoso4510@gmail.com>
2025-07-31 17:39:31 -0300

    MethodReferences

---
+++ src/javacore/introducao/dominio/ZZClambdas/test/MethodReferenceTest04.java
@@ -0,0 +1,28 @@
+package javacore.introducao.dominio.ZZClambdas.test;
+
+import javacore.introducao.dominio.ZZClambdas.dominio.Anime;
+import javacore.introducao.dominio.ZZClambdas.services.AnimeComparators;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+public class MethodReferenceTest04 {
+    public static void main(String[] args) {
+        Supplier<AnimeComparators> newAnimeComparators = AnimeComparators::new;
+        AnimeComparators animeComparators = newAnimeComparators.get();
+
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berseck", 5), new Anime("One Piece", 900), new Anime("Naruto", 500)));
+
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+
+        System.out.println(animeList);
+
+        BiFunction<String, Integer, Anime> animeBiFunction = (title, episodes) -> new Anime(title, episodes);
+        BiFunction<String, Integer, Anime> animeBiFunction2 = Anime::new;
+
+        System.out.println(animeBiFunction2.apply("Super campeões", 36));
+    }
+}
