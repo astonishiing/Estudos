0fa56648bdeac466eda92f23aea797b6564779d6
astonishiing <wcardoso4510@gmail.com>
2025-08-01 15:32:03 -0300

    OptionalTest02

--- src/javacore/introducao/dominio/ZZDoptional/test/OptionalTest02.java
+++ src/javacore/introducao/dominio/ZZDoptional/test/OptionalTest02.java
@@ -1,4 +1,25 @@
 package javacore.introducao.dominio.ZZDoptional.test;
 
+import javacore.introducao.dominio.ZZDoptional.dominio.Manga;
+import javacore.introducao.dominio.ZZDoptional.repositorio.MangaRepository;
+
+import java.util.Optional;
+
 public class OptionalTest02 {
+    public static void main(String[] args) {
+        Optional<Manga> mangaByTitle = MangaRepository.finalByTitle("Boku no Hero");
+
+        mangaByTitle.ifPresent(m -> m.setTitle("Boku no Hero 2"));
+        System.out.println(mangaByTitle);
+
+        Manga mangaById = MangaRepository.findById(2).
+                orElseThrow(IllegalArgumentException::new);
+
+        System.out.println(mangaById);
+
+        Manga newManga = MangaRepository.finalByTitle("Drifters").
+                orElseGet(() -> new Manga(3, "Drifters", 20));
+
+        System.out.println(newManga);
+    }
 }
