f04a0f29ec0f3e1f8eef58c7b5b6eb02ec46567d
astonishiing <wcardoso4510@gmail.com>
2025-08-01 16:06:28 -0300

    StreamTest02

---
+++ src/javacore/introducao/dominio/ZZEstreams/test/StreamTest02.java
@@ -0,0 +1,32 @@
+package javacore.introducao.dominio.ZZEstreams.test;
+
+import javacore.introducao.dominio.ZZEstreams.classes.LightNovel;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamTest02 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 3.89),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no Life", 2.89),
+            new LightNovel("Fillmetal Alchesmist", 5.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+
+    public static void main(String[] args) {
+        List<String> titles = lightNovels.stream()
+                .sorted(Comparator.comparing(LightNovel::getTitle))
+                .filter(ln -> ln.getPrice() <= 4)
+                .limit(3)
+                .map(LightNovel::getTitle)
+                .collect(Collectors.toList());
+
+        System.out.println(titles);
+    }
+}
