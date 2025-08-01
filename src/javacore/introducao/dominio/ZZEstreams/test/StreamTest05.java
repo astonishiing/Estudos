5743771ec433d28d0bbae0a23539943b172766e1
astonishiing <wcardoso4510@gmail.com>
2025-08-01 18:29:42 -0300

    StreamTest05

---
+++ src/javacore/introducao/dominio/ZZEstreams/test/StreamTest05.java
@@ -0,0 +1,11 @@
+package javacore.introducao.dominio.ZZEstreams.test;
+
+import java.util.List;
+
+public class StreamTest05 {
+    public static void main(String[] args) {
+        List<String> words = List.of("Goku", "Goku", "No", "Mi");
+
+
+    }
+}
