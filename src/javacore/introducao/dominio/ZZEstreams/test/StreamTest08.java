376aa11412a48da9fd3c55dc0d5a4d944711a77a
astonishiing <wcardoso4510@gmail.com>
2025-08-03 20:48:50 -0300

    StreamTest08

---
+++ src/javacore/introducao/dominio/ZZEstreams/test/StreamTest08.java
@@ -0,0 +1,29 @@
+package javacore.introducao.dominio.ZZEstreams.test;
+
+import javacore.introducao.dominio.ZZEstreams.classes.LightNovel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StreamTest08 {
+
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99),
+            new LightNovel("Overlord", 10.99),
+            new LightNovel("Violet Evergarden", 5.99),
+            new LightNovel("No Game no Life", 2.89),
+            new LightNovel("Fillmetal Alchesmist", 5.99),
+            new LightNovel("Kumo desuga", 1.99),
+            new LightNovel("Monogatari", 4.00)
+    ));
+
+
+    public static void main(String[] args) {
+        lightNovels.stream()
+                .map(LightNovel::getPrice)
+                .filter(price -> price > 3)
+                .reduce(Double::sum)
+                .ifPresent(System.out::println);
+
+    }
+}
