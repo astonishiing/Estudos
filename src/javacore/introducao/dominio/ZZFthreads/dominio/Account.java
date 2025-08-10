0747569ab1d6ab0657cc0b4faf01d39a9fa1c9de
astonishiing <wcardoso4510@gmail.com>
2025-08-10 10:00:37 -0300

    Account

--- src/javacore/introducao/dominio/ZZFthreads/dominio/Account.java
+++ src/javacore/introducao/dominio/ZZFthreads/dominio/Account.java
@@ -1,4 +1,13 @@
 package javacore.introducao.dominio.ZZFthreads.dominio;
 
 public class Account {
+    private int balance = 50;
+
+    public void withDrawl(int amount) {
+        this.balance = this.balance - amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
 }
