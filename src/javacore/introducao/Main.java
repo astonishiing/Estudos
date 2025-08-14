7dea4a299789e30149f3dea30a15ebc93b929a42
astonishiing <wcardoso4510@gmail.com>
2025-08-14 17:14:02 -0300

    Heranca

---
+++ src/javacore/introducao/Main.java
@@ -0,0 +1,16 @@
+package javacore.introducao;
+
+public class Main {
+    public static void main(String[] args) {
+        Employeer employeer = new Employeer();
+        Manager manager = new Manager();
+
+        manager.setName("Joao");
+        manager.setLogin("joao");
+        manager.setPassword("123456");
+
+        System.out.println(manager.getName());
+        System.out.println(manager.getLogin());
+        System.out.println(manager.getPassword());
+    }
+}
---
+++ src/javacore/introducao/Manager.java
@@ -0,0 +1,34 @@
+package javacore.introducao;
+
+public class Manager extends Employeer {
+
+    private String login;
+
+    private String password;
+
+    private double comission;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getComission() {
+        return comission;
+    }
+
+    public void setComission(double comission) {
+        this.comission = comission;
+    }
+}
