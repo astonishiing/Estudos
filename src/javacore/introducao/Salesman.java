7395283ca6d56210dd65fe07faff3387d7e690d9
astonishiing <wcardoso4510@gmail.com>
2025-08-16 14:22:41 -0300

    Salesman

--- src/javacore/introducao/Salesman.java
+++ src/javacore/introducao/Salesman.java
@@ -4,13 +4,34 @@ public non-sealed class Salesman extends Employeer {
 
     private double percentPerSold;
 
-    public Salesman(String code, String name, String address, int age, int salary, double percentPerSold) {
+    private double soldAmount;
+
+    public Salesman(String code, String name, String address, int age, int salary, double percentPerSold, double soldAmount) {
         super(code, name, address, age, salary);
         this.percentPerSold = percentPerSold;
+        this.soldAmount = soldAmount;
     }
 
     public Salesman() {
 
+    }
+
+    @Override
+    public String getCode() {
+        return "SL" + super.getCode();
+    }
+
+    @Override
+    public double getFullSalary() {
+        return this.salary + (soldAmount * percentPerSold) / 100;
+    }
+
+    public double getSoldAmount() {
+        return soldAmount;
+    }
+
+    public void setSoldAmount(double soldAmount) {
+        this.soldAmount = soldAmount;
     }
 
     public double getPercentPerSold() {
