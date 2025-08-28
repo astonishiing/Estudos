e6777d0136379f601287edb22fa86b231f5386c2
astonishiing <wcardoso4510@gmail.com>
2025-08-28 14:15:18 -0300

    ExMedia02

--- src/javacore/introducao/dominio/Exercicios/exercicios/ExMedia02.java
+++ src/javacore/introducao/dominio/Exercicios/exercicios/ExMedia02.java
@@ -1,4 +1,24 @@
 package javacore.introducao.dominio.Exercicios.exercicios;
 
+import java.util.Scanner;
+
 public class ExMedia02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double A;
+        double B;
+        double C;
+
+        double pesoA = 2;
+        double pesoB = 3;
+        double pesoC = 5;
+
+        A = input.nextDouble();
+        B = input.nextDouble();
+        C = input.nextDouble();
+
+        double media = (A * pesoA + B * pesoB + C * pesoC) / (pesoA + pesoB + pesoC);
+        System.out.printf("MEDIA = %.1f%n", media);
+    }
 }
