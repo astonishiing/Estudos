8a8955f4b5bfb4f1eee5c81242402f56c6f39899
astonishiing <wcardoso4510@gmail.com>
2025-08-28 14:05:50 -0300

    ExMedia

--- src/javacore/introducao/dominio/Exercicios/exercicios/App.java
+++ src/javacore/introducao/dominio/Exercicios/exercicios/App.java
@@ -1,4 +1,25 @@
 package javacore.introducao.dominio.Exercicios.exercicios;
 
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double A;
+        double B;
+
+        A = input.nextDouble();
+        B = input.nextDouble();
+
+        double pesoA = 3.5;
+        double pesoB = 7.5;
+
+        double media = (A * pesoA + B * pesoB) / (pesoA + pesoB);
+
+        if(A <= 10.0 && B <= 10.0){
+            System.out.printf("MEDIA = %.5f", media);
+        }else {
+            System.out.printf("Presentation Error");
+        }
+    }
 }
