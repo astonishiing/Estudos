a3999bcb6b3072d11bd00f13903d1c673bc657de
astonishiing <wcardoso4510@gmail.com>
2025-09-13 09:03:20 -0300

    Forma simplificada de Lambda 03

--- src/javacore/introducao/dominio/Minterfaces/Interface_Funcional/Main.java
+++ src/javacore/introducao/dominio/Minterfaces/Interface_Funcional/Main.java
@@ -2,13 +2,13 @@ package javacore.introducao.dominio.Minterfaces.Interface_Funcional;
 
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Main {
     public static void main(String[] args) {
         List<User> users = List.of(new User("Maria", 21), new User("Joao", 32), new User("Eduardo", 40)
             , new User("Ana", 19));
 
-            users.forEach(user -> System.out.println(user));
-
+            users.forEach(System.out::println);
     }
 }
