27bc3a5acc535619ae134b5c682b1453cc85f111
astonishiing <wcardoso4510@gmail.com>
2025-09-13 09:06:57 -0300

    Impressão de idades com ValueOf

--- src/javacore/introducao/dominio/Minterfaces/Interface_Funcional/Main.java
+++ src/javacore/introducao/dominio/Minterfaces/Interface_Funcional/Main.java
@@ -9,6 +9,10 @@ public class Main {
         List<User> users = List.of(new User("Maria", 21), new User("Joao", 32), new User("Eduardo", 40)
             , new User("Ana", 19));
 
-            users.forEach(System.out::println);
+           printStringValues( user -> String.valueOf(user.age()),users);
+    }
+    private static void printStringValues(Function<User, String> callback, List<User> users){
+        users.forEach(u -> System.out.println(callback.apply(u)));
+
     }
 }
