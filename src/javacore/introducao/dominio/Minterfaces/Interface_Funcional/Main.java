4f852ba55ff408cd9cd7a1e06d25cce3444f8288
astonishiing <wcardoso4510@gmail.com>
2025-09-13 08:47:15 -0300

    Formas de impressão das informações na classe anonima com interfaces

--- src/javacore/introducao/dominio/Minterfaces/Interface_Funcional/Main.java
+++ src/javacore/introducao/dominio/Minterfaces/Interface_Funcional/Main.java
@@ -1,11 +1,19 @@
 package javacore.introducao.dominio.Minterfaces.Interface_Funcional;
 
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
         List<User> users = List.of(new User("Maria", 21), new User("Joao", 32), new User("Eduardo", 40)
             , new User("Ana", 19));
+            var consumer = new Consumer<User>() {
+                @Override
+                public void accept(User user) {
+                    System.out.println(user);
+                }
+            };
+            users.forEach(consumer);
 
     }
 }
