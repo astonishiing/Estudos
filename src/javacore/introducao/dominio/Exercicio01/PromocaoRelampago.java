8a9fd406582090c6df03ec6e84edc9cd60220c53
astonishiing <wcardoso4510@gmail.com>
2025-09-14 17:19:22 -0300

    Exer PromoRelam

--- src/javacore/introducao/dominio/Exercicio01/PromocaoRelampago.java
+++ src/javacore/introducao/dominio/Exercicio01/PromocaoRelampago.java
@@ -10,5 +10,24 @@ public class PromocaoRelampago {
         System.out.println(calcularDesconto(valorCompra));
         input.close();
     }
-    public static String calcularDesconto(String valorCompra)
+    public static String calcularDesconto(String valorCompra){
+        BigDecimal valor = new BigDecimal(valorCompra);
+        BigDecimal descontoPercentual;
+        BigDecimal limite1 = new BigDecimal("50.00");
+        BigDecimal limite2 = new BigDecimal("100.00");
+
+        if (valor.compareTo(limite1) < 0 ) {
+            descontoPercentual = BigDecimal.ZERO;
+        }
+
+        else if(valor.compareTo(limite1) >= 0 && valor.compareTo(limite2) <= 0){
+            descontoPercentual = new BigDecimal("10");
+        }
+
+        else {
+            descontoPercentual = new BigDecimal("20");
+        }
+
+        return "Desconto de " + descontoPercentual + "%";
+    }
 }
