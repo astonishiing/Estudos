1d50ecea0089e82b0714e767e0ee2d9d80a84e09
astonishiing <wcardoso4510@gmail.com>
2025-09-14 09:42:23 -0300

    PromocaoRelampago exerci

---
+++ src/javacore/introducao/dominio/Exercicio01/PromocaoRelampago.java
@@ -0,0 +1,14 @@
+package javacore.introducao.dominio.Exercicio01;
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+
+public class PromocaoRelampago {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String valorCompra = input.nextLine();
+        System.out.println(calcularDesconto(valorCompra));
+        input.close();
+    }
+    public static String calcularDesconto(String valorCompra)
+}
