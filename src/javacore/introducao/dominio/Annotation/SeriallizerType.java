3ec27f6f9edf83f4d3f61939c6043a471ac20892
astonishiing <wcardoso4510@gmail.com>
2025-09-14 17:46:15 -0300

    Criacao do FieldFormat

--- src/javacore/introducao/dominio/Annotation/SeriallizerType.java
+++ src/javacore/introducao/dominio/Annotation/SeriallizerType.java
@@ -1,5 +1,15 @@
 package javacore.introducao.dominio.Annotation;
 
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RUNTIME) //Funcionar em RUNTIME
+@Target(TYPE) //Alvo em que ela vai ser utilizada
 public @interface SeriallizerType {
 
+    FielFormatEnum fieldFormat();
 }
