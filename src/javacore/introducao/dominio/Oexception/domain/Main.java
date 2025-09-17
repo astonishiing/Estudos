595bee859b474358a64a7ebcfd4d2de5dccb3835
astonishiing <wcardoso4510@gmail.com>
2025-09-17 14:09:18 -0300

    Comments

--- src/javacore/introducao/dominio/Oexception/domain/Main.java
+++ src/javacore/introducao/dominio/Oexception/domain/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("terminou");
     }
 
+    // Exemplo ilustrativo das exceptions
     private static void test(){
                     new Throwable(); //Pai de exceptions e errors
                     /                         \
