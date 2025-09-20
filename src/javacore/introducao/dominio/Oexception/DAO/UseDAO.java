4370845e05606a12b864d3c15f8c832853a50448
astonishiing <wcardoso4510@gmail.com>
2025-09-20 08:46:08 -0300

    Tratando exceções

--- src/javacore/introducao/dominio/Oexception/DAO/UseDAO.java
+++ src/javacore/introducao/dominio/Oexception/DAO/UseDAO.java
@@ -1,5 +1,16 @@
 package javacore.introducao.dominio.Oexception.DAO;
 
+import javacore.introducao.dominio.Oexception.Model.UserModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class UseDAO {
+
+    private List<UserModel> models = new ArrayList<>();
+
+    public UserModel save(final UserModel model){
+
+    }
 
 }
--- src/javacore/introducao/dominio/Oexception/Model/UserModel.java
+++ src/javacore/introducao/dominio/Oexception/Model/UserModel.java
@@ -2,5 +2,9 @@ package javacore.introducao.dominio.Oexception.Model;
 
 import java.time.OffsetDateTime;
 
-public record UserModel(long id, String name, String email, OffsetDateTime birthday)  {
+public class UserModel  {
+    long id;
+    String name;
+    String email;
+    OffsetDateTime birthday;
 }
