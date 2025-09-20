8c4a24a16b5d87b1c9ded99437d4f72609bee43e
astonishiing <wcardoso4510@gmail.com>
2025-09-20 10:22:21 -0300

    Main

--- src/javacore/introducao/dominio/Oexception/Model/MenuOption.java
+++ src/javacore/introducao/dominio/Oexception/Model/MenuOption.java
@@ -1,10 +1,21 @@
 package javacore.introducao.dominio.Oexception.Model;
 
+import javacore.introducao.dominio.Oexception.DAO.UserDAO;
+
+import java.util.function.Consumer;
+
 public enum MenuOption {
     SAVE,
+
     UPDATE,
+
     DELETE,
+
     FIND_BY_ID,
+
     FIND_ALL,
+
     EXIT;
+
+
 }
--- src/javacore/introducao/dominio/Oexception/domain/Main.java
+++ src/javacore/introducao/dominio/Oexception/domain/Main.java
@@ -2,28 +2,103 @@ package javacore.introducao.dominio.Oexception.domain;
 
 import javacore.introducao.dominio.Oexception.DAO.UserDAO;
 import javacore.introducao.dominio.Oexception.Model.MenuOption;
+import javacore.introducao.dominio.Oexception.Model.UserModel;
 
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 
-    private static UserDAO userDAO = new UserDAO();
+    private static final UserDAO userDAO = new UserDAO();
+
+    private final static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Bem-vindo ao cadastro de usuários, selecione a operação desejada");
-        System.out.println("1 - Cadastrar");
-        System.out.println("2 - Atualizar");
-        System.out.println("3 - Excluir");
-        System.out.println("4 - Buscar por identificador");
-        System.out.println("5 - Listar");
-        System.out.println("6 - Sair");
-        var userInput = scanner.nextInt();
         while(true) {
+            System.out.println("Bem-vindo ao cadastro de usuários, selecione a operação desejada");
+            System.out.println("1 - Cadastrar");
+            System.out.println("2 - Atualizar");
+            System.out.println("3 - Excluir");
+            System.out.println("4 - Buscar por identificador");
+            System.out.println("5 - Listar");
+            System.out.println("6 - Sair");
+            var userInput = scanner.nextInt();
             MenuOption selectedOption = MenuOption.values()[userInput - 1];
+            switch (selectedOption){
+                case SAVE -> {
+                    UserModel user = userDAO.save(requestToSave());
+                    System.out.printf("Usuário cadastrado %s", user);
+                }
 
+                case UPDATE -> {
+                    UserModel user = userDAO.update(requestToUpdate());
+                    System.out.printf("Usuario atualizado %s", user);
+                }
+
+                case DELETE -> {
+                    userDAO.delete(requestId());
+                    System.out.println("Usuario excluído");
+                }
+
+                case FIND_BY_ID -> {
+                    long id = requestId();
+                    UserModel byId = userDAO.findById(id);
+                    System.out.printf("Usuário com id %s: ", id);
+                    System.out.println(byId);
+
+                }
+                case FIND_ALL -> {
+                    List<UserModel> all = userDAO.findAll();
+                    System.out.println("Usuários cadastrados:");
+                    System.out.println("===============");
+                    all.forEach(System.out::println);
+                    System.out.println("=======fim========");
+                }
+                case EXIT -> System.exit(0);
+            }
         }
     }
 
+    private static long requestId(){
+        System.out.println("Informe o identificador do usuário");
+        return scanner.nextLong();
+    }
+
+    private static UserModel requestToSave(){
+        System.out.println("Informe o nome do usuário");
+        var name = scanner.next();
+
+        System.out.println("Informe o email do usuário");
+        var email = scanner.next();
+
+        System.out.println("Informe a data de nascimento do usuário (dd/MM/yyyy)");
+        var birthdayString = scanner.next();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        var birthday = OffsetDateTime.parse(birthdayString, formatter);
+
+        return new UserModel(0, name, email, birthday);
+    }
+
+    private static UserModel requestToUpdate(){
+        System.out.println("Informe o identificador do usuário");
+        var id = scanner.nextLong();
+
+        System.out.println("Informe o nome do usuário");
+        var name = scanner.next();
+
+        System.out.println("Informe o email do usuário");
+        var email = scanner.next();
+
+        System.out.println("Informe a data de nascimento do usuário (dd/MM/yyyy)");
+        var birthdayString = scanner.next();
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        var birthday = OffsetDateTime.parse(birthdayString, formatter);
+
+        return new UserModel(id, name, email, birthday);
+    }
 
 }
