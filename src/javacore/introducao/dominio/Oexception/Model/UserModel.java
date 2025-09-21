7677f9bdc359a12073bcc1d5e25c57f0c37257a1
astonishiing <wcardoso4510@gmail.com>
2025-09-21 20:59:13 -0300

    Alteracao de variavel de referencia

--- src/javacore/introducao/dominio/Oexception/Model/UserModel.java
+++ src/javacore/introducao/dominio/Oexception/Model/UserModel.java
@@ -1,5 +1,6 @@
 package javacore.introducao.dominio.Oexception.Model;
 
+import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.Objects;
 
@@ -7,13 +8,13 @@ public class UserModel {
     private long id;
     private String name;
     private String email;
-    private OffsetDateTime birthday;
+    private LocalDate birthday;
 
     public UserModel(){
     }
 
 
-    public UserModel(long id, String name, String email, OffsetDateTime birthday) {
+    public UserModel(long id, String name, String email, LocalDate birthday) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -44,11 +45,11 @@ public class UserModel {
         this.email = email;
     }
 
-    public OffsetDateTime getBirthday() {
+    public LocalDate getBirthday() {
         return birthday;
     }
 
-    public void setBirthday(OffsetDateTime birthday) {
+    public void setBirthday(LocalDate birthday) {
         this.birthday = birthday;
     }
 
