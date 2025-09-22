4089d4ef550faaf4926fb4b825c2bc6315985271
astonishiing <wcardoso4510@gmail.com>
2025-09-21 21:00:15 -0300

    Main

--- src/javacore/introducao/dominio/Oexception/domain/Main.java
+++ src/javacore/introducao/dominio/Oexception/domain/Main.java
@@ -1,17 +1,21 @@
 package javacore.introducao.dominio.Oexception.domain;
 
 import javacore.introducao.dominio.Gassociacao.dominio.Local;
+import javacore.introducao.dominio.Minterfaces.Interface_Funcional.User;
 import javacore.introducao.dominio.Oexception.DAO.UserDAO;
 import javacore.introducao.dominio.Oexception.Model.MenuOption;
 import javacore.introducao.dominio.Oexception.Model.UserModel;
 import javacore.introducao.dominio.Oexception.exception.EmptyStorageException;
 import javacore.introducao.dominio.Oexception.exception.UserNotFoundException;
+import javacore.introducao.dominio.Oexception.exception.ValidatorException;
 
 import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
+
+import static javacore.introducao.dominio.Oexception.validator.UserValidator.verifyModel;
 
 public class Main {
 
@@ -32,8 +36,14 @@ public class Main {
             MenuOption selectedOption = MenuOption.values()[userInput - 1];
             switch (selectedOption){
                 case SAVE -> {
-                    UserModel user = userDAO.save(requestToSave());
-                    System.out.printf("Usuário cadastrado %s", user);
+                    try {
+                        UserModel user = userDAO.save(requestToSave());
+                        System.out.printf("Usuário cadastrado %s", user);
+                    }catch (ValidatorException ex) {
+                        System.out.println(ex.getMessage());
+                        ex.printStackTrace();
+                    }
+
                 }
 
                 case UPDATE -> {
@@ -42,7 +52,11 @@ public class Main {
                         System.out.printf("Usuario atualizado %s", user);
                     }catch (UserNotFoundException | EmptyStorageException ex){
                         System.out.println(ex.getMessage());
-                    }finally{
+                    }catch (ValidatorException ex){
+                        System.out.println(ex.getMessage());
+                        ex.printStackTrace();
+                    }
+                    finally{
                         System.out.println("================");
                     }
                 }
@@ -86,7 +100,7 @@ public class Main {
         return scanner.nextLong();
     }
 
-    private static UserModel requestToSave(){
+    private static UserModel requestToSave() throws ValidatorException {
         System.out.println("Informe o nome do usuário");
         var name = scanner.next();
         System.out.println("Informe o email do usuário");
@@ -95,10 +109,17 @@ public class Main {
         var birthdayString = scanner.next();
         var formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         var birthday = LocalDate.parse(birthdayString, formatter);
-        return new UserModel(0, name, email, birthday);
+        UserModel user = new UserModel(0, name, email, birthday);
+        return validateInputs(0, name,email, birthday);
     }
 
-    private static UserModel requestToUpdate(){
+    private static UserModel validateInputs(final long id, final String name, final String email, final LocalDate birthday) throws ValidatorException{
+        var user = new UserModel(0, name, email, birthday);
+        verifyModel(user);
+        return user;
+    }
+
+    private static UserModel requestToUpdate() throws ValidatorException {
         System.out.println("Informe o identificador do usuário");
         var id = scanner.nextLong();
 
@@ -114,7 +135,7 @@ public class Main {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         var birthday = LocalDate.parse(birthdayString, formatter);
 
-        return new UserModel(id, name, email, birthday);
+        return validateInputs(0, name,email, birthday);
     }
 
 }
