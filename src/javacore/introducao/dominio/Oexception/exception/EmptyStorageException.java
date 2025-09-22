9b4cb5c563a379465907746a73ef860908db5746
astonishiing <wcardoso4510@gmail.com>
2025-09-21 21:00:01 -0300

    EmptyStorage

--- src/javacore/introducao/dominio/Oexception/exception/EmptyStorageException.java
+++ src/javacore/introducao/dominio/Oexception/exception/EmptyStorageException.java
@@ -1,7 +1,7 @@
 package javacore.introducao.dominio.Oexception.exception;
 
 public class EmptyStorageException extends RuntimeException {
-  public EmptyStorageException(String message) {
-    super(message);
-  }
+    public EmptyStorageException(String message) {
+        super(message);
+    }
 }
