c286d9db189107e3d3264b0018fc91e9805a1eed
astonishiing <wcardoso4510@gmail.com>
2025-10-13 11:14:23 -0300

    FutureTest

---
+++ src/javacore/introducao/dominio/ZZGconcorrencia/test/FutureTest01.java
@@ -0,0 +1,26 @@
+package javacore.introducao.dominio.ZZGconcorrencia.test;
+
+import java.util.concurrent.*;
+
+public class FutureTest01 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        Future<Double> dollarRequest = executorService.submit(() -> {
+            TimeUnit.SECONDS.sleep(5);
+            return 4.35D;
+        });
+        System.out.println(doingSomething());
+        Double dollarResponse = dollarRequest.get(3, TimeUnit.SECONDS);
+        System.out.println("Dollar: " + dollarResponse);
+        executorService.shutdown();
+    }
+
+    public static long doingSomething(){
+        System.out.println(Thread.currentThread().getName());
+        long sum = 0;
+        for (int i = 0; i < 1_000_000; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+}
