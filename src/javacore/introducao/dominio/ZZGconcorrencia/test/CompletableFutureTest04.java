a3ec3013beab90a08b346755dc3f4143a98cfa84
astonishiing <wcardoso4510@gmail.com>
2025-10-13 18:52:21 -0300

    CompletableFutureTest04

--- src/javacore/introducao/dominio/ZZGconcorrencia/test/CompletableFutureTest04.java
+++ src/javacore/introducao/dominio/ZZGconcorrencia/test/CompletableFutureTest04.java
@@ -1,4 +1,23 @@
 package javacore.introducao.dominio.ZZGconcorrencia.test;
 
+import javacore.introducao.dominio.ZZGconcorrencia.dominio.Quote;
+import javacore.introducao.dominio.ZZGconcorrencia.service.StoreServiceWithDiscount;
+
+import java.util.List;
+
 public class CompletableFutureTest04 {
+    public static void main(String[] args) {
+        StoreServiceWithDiscount storeServiceWithDiscount = new StoreServiceWithDiscount();
+        searchPricesWithDiscount(storeServiceWithDiscount);
+    }
+
+    private static void searchPricesWithDiscount(StoreServiceWithDiscount service){
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+//        stores.forEach(s -> System.out.println(service.getPrinceSync(s)));
+        stores.stream()
+                .map(service::getPrinceSync)//store:price:code
+                .map(Quote::newQuote)
+                .map(service::applyDiscount)
+                .forEach(System.out::println);
+    }
 }
