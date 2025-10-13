f97a7fe84305e210e0ae2941b0fa2698b12919ce
astonishiing <wcardoso4510@gmail.com>
2025-10-13 18:52:03 -0300

    StoreServiceWithDiscount

--- src/javacore/introducao/dominio/ZZGconcorrencia/service/StoreServiceWithDiscount.java
+++ src/javacore/introducao/dominio/ZZGconcorrencia/service/StoreServiceWithDiscount.java
@@ -1,28 +1,46 @@
 package javacore.introducao.dominio.ZZGconcorrencia.service;
 
-import java.util.concurrent.*;
+import javacore.introducao.dominio.ZZGconcorrencia.dominio.Discount;
+import javacore.introducao.dominio.ZZGconcorrencia.dominio.Quote;
 
-public class StoreServiceDeprecated {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
+
+public class StoreServiceWithDiscount {
 
     private static final ExecutorService ex = Executors.newCachedThreadPool();
 
-    public double getPrinceSync(String storeName){
-        System.out.printf("Getting prices sync for store %s%n", storeName);
-        return priceGenerator();
+    public String getPrinceSync(String storeName) {
+        double price = priceGenerator();
+        Discount.Code discountCode = Discount.Code.values()[
+                ThreadLocalRandom.current().nextInt(Discount.Code.values().length)
+                ];
+        return String.format("%s:%.2f:%s", storeName, price, discountCode);
     }
 
-    private double priceGenerator(){
-        System.out.printf("%s generating price %n", Thread.currentThread().getName());
+    public String applyDiscount(Quote quote) {
+        delay();
+        double discountValue = quote.getPrice() * (100 - quote.getDiscountCode().getPercentage() / 100);
+        return String.format("'%s' original price: '%.2f'. Applying Discount code '%s'. Final price: '%.2f'", quote.getStore(),
+                quote.getPrice(),
+                quote.getDiscountCode(),
+                discountValue);
+    }
+
+    private double priceGenerator() {
         delay();
         return ThreadLocalRandom.current().nextInt(1, 500) * 10;
     }
 
-    public static void shutdown(){
+    public static void shutdown() {
         ex.shutdown();
     }
-    private void delay(){
+
+    private void delay() {
         try {
-            TimeUnit.SECONDS.sleep(2);
+            TimeUnit.SECONDS.sleep(1);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
