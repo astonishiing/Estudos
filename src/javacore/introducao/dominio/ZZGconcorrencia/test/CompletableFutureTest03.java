ba3ca1933bec0d7c84f8eca32297d54b567d18a4
astonishiing <wcardoso4510@gmail.com>
2025-10-13 18:16:33 -0300

    CompletableFutureTest03

---
+++ src/javacore/introducao/dominio/ZZGconcorrencia/test/CompletableFutureTest03.java
@@ -0,0 +1,44 @@
+package javacore.introducao.dominio.ZZGconcorrencia.test;
+
+import javacore.introducao.dominio.ZZGconcorrencia.service.StoreService;
+import javacore.introducao.dominio.ZZGconcorrencia.service.StoreServiceDeprecated;
+
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class CompletableFutureTest03 {
+    public static void main(String[] args) {
+        StoreServiceDeprecated storeServiceDeprecated = new StoreServiceDeprecated();
+//        searchPrinceSync(storeService);
+//        searchPricesAsyncFuture(storeService);
+        searchPricesAsyncCompletableFuture(storeServiceDeprecated);
+
+    }
+
+    private static void searchPricesAsyncCompletableFuture(StoreServiceDeprecated storeService) {
+        long start = System.currentTimeMillis();
+        ExecutorService executor = Executors.newFixedThreadPool(10, r -> {
+            Thread thread = new Thread(r);
+            thread.setDaemon(true);
+            return thread;
+        });
+
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+
+        List<CompletableFuture<Double>> completableFutures = stores.stream()
+                .map(s -> CompletableFuture.supplyAsync(() -> storeService.getPrinceSync(s), executor))
+                .collect(Collectors.toList());
+
+        List<Double> prices = completableFutures.stream()
+                .map(CompletableFuture::join)
+                .collect(Collectors.toList());
+
+        System.out.println(prices);
+
+        long end = System.currentTimeMillis();
+        executor.shutdown();
+        System.out.printf("Time passed to searchPrinceSync %dms%n", (end - start));
+    }
+}
