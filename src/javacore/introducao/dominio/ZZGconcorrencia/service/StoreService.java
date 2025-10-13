e5a2bc6bd2ca48bf3d139dad437c5f1abfb8ba26
astonishiing <wcardoso4510@gmail.com>
2025-10-13 12:54:41 -0300

    StoreService

--- src/javacore/introducao/dominio/ZZGconcorrencia/service/StoreService.java
+++ src/javacore/introducao/dominio/ZZGconcorrencia/service/StoreService.java
@@ -18,6 +18,11 @@ public class StoreService {
         return ex.submit(this::priceGenerator);
     }
 
+    public CompletableFuture<Double> getPricesAsyncCompletableFuture(String storeName) {
+        System.out.printf("Getting prices sync for store %s%n", storeName);
+        return CompletableFuture.supplyAsync(this::priceGenerator);
+    }
+
     private double priceGenerator(){
         System.out.printf("%s generating price %n", Thread.currentThread().getName());
         delay();
