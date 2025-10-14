961347db605a76821938a3360101649bff879f72
astonishiing <wcardoso4510@gmail.com>
2025-10-14 20:44:00 -0300

    encadeando chamadas

--- src/javacore/introducao/dominio/ZZGconcorrencia/test/CompletableFutureTest04.java
+++ src/javacore/introducao/dominio/ZZGconcorrencia/test/CompletableFutureTest04.java
@@ -4,11 +4,14 @@ import javacore.introducao.dominio.ZZGconcorrencia.dominio.Quote;
 import javacore.introducao.dominio.ZZGconcorrencia.service.StoreServiceWithDiscount;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+
 
 public class CompletableFutureTest04 {
     public static void main(String[] args) {
         StoreServiceWithDiscount storeServiceWithDiscount = new StoreServiceWithDiscount();
-        searchPricesWithDiscount(storeServiceWithDiscount);
+        searchPricesWithDiscountAsync(storeServiceWithDiscount);
     }
 
     private static void searchPricesWithDiscount(StoreServiceWithDiscount service){
@@ -20,6 +23,27 @@ public class CompletableFutureTest04 {
                 .map(Quote::newQuote)
                 .map(service::applyDiscount)
                 .forEach(System.out::println);
+        long end = System.currentTimeMillis();
+        System.out.printf("Time passed to searchPrinceSync %dms%n", (end - start));
+    }
+
+    private static void searchPricesWithDiscountAsync(StoreServiceWithDiscount service){
+        long start = System.currentTimeMillis();
+        List<String> stores = List.of("Store 1", "Store 2", "Store 3", "Store 4");
+
+        List<CompletableFuture<String>> completableFutures = stores.stream()
+                // Getting the price async storeName:price:discountCode
+                .map(s -> CompletableFuture.supplyAsync(() -> service.getPrinceSync(s)))
+                // Instantiating a new quote from the string genereated by getPriceSync
+                .map(cf -> cf.thenApply(Quote::newQuote))
+                // Composing the first commpletable future
+                .map(cf -> cf.thenCompose(quote -> CompletableFuture.supplyAsync(() -> service.applyDiscount(quote))))
+                .collect(Collectors.toList());
+
+        completableFutures.stream()
+                .map(CompletableFuture::join)
+                .forEach(System.out::println);
+
         long end = System.currentTimeMillis();
         System.out.printf("Time passed to searchPrinceSync %dms%n", (end - start));
     }
