175ce273945a195c10fb45954dfefc357ef8ff58
astonishiing <wcardoso4510@gmail.com>
2025-11-02 20:24:55 -0300

    Varargs

--- src/javacore/introducao/dominio/Bintroducaometodos/metodos1/Calculadora.java
+++ src/javacore/introducao/dominio/Bintroducaometodos/metodos1/Calculadora.java
@@ -9,6 +9,7 @@ public class Calculadora {
     public void subtraiDoisNumeros(){
         System.out.println(21 - 2);
     }
+    // Parametros nada mais são do que variaveis locais, durante o contexto do método
     public void multiplicaDoisNumeros(int num, int num2){
         System.out.println(num * num2);
     }
@@ -32,5 +33,30 @@ public class Calculadora {
         System.out.println("Dentro do altera dois numeros");
         System.out.println("num9: " + numero1);
         System.out.println("num3: " + numero3);
+    }
+
+    //O que muda de um para o outro é a chamada (do somaArray e somaVarArgs)
+    public void somaArray(int[] numeros){
+        int soma = 0;
+        for (int num : numeros){
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+
+    public void somaVarArgs(int... numeros){ //Varargs
+        int soma = 0;
+        for (int num : numeros){
+            soma += num;
+        }
+        System.out.println(soma);
+    }
+
+    public void somaVarArgs2(double valor,int... numeros){ //Varargs
+        int soma = 0;
+        for (int num : numeros){
+            soma += num;
+        }
+        System.out.println(soma);
     }
 }
---
+++ src/javacore/introducao/dominio/Bintroducaometodos/metodos1/testmetodos/CalculadoraTest05.java
@@ -0,0 +1,4 @@
+package javacore.introducao.dominio.Bintroducaometodos.metodos1.testmetodos;
+
+public class CalculadoraTest05 {
+}
