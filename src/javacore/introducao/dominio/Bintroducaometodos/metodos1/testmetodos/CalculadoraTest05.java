0e98bed2567d62c251653e02dcdac37564ac47fb
astonishiing <wcardoso4510@gmail.com>
2025-11-02 20:25:53 -0300

    CalculadoraTest05

--- src/javacore/introducao/dominio/Bintroducaometodos/metodos1/testmetodos/CalculadoraTest05.java
+++ src/javacore/introducao/dominio/Bintroducaometodos/metodos1/testmetodos/CalculadoraTest05.java
@@ -1,4 +1,13 @@
 package javacore.introducao.dominio.Bintroducaometodos.metodos1.testmetodos;
 
+import javacore.introducao.dominio.Bintroducaometodos.metodos1.Calculadora;
+
 public class CalculadoraTest05 {
+    public static void main(String... args) {
+        Calculadora calculadora = new Calculadora();
+        int[] numeros = {1,2,3,4,5};
+        calculadora.somaArray(numeros);
+        calculadora.somaVarArgs(numeros);
+        calculadora.somaVarArgs(1,2,3,4,5);
+    }
 }
