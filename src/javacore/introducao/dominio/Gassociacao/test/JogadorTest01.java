189d995eb7686a6b8e4a9730d9cfffd9d9985dd7
astonishiing <wcardoso4510@gmail.com>
2025-11-08 08:15:08 -0300

    Explicacao das outras formas de referência um array

--- src/javacore/introducao/dominio/Gassociacao/test/JogadorTest01.java
+++ src/javacore/introducao/dominio/Gassociacao/test/JogadorTest01.java
@@ -7,6 +7,9 @@ public class JogadorTest01 {
         Jogador jogador1 = new Jogador("Pele");
         Jogador jogador2 = new Jogador("Romario");
         Jogador jogador3 = new Jogador("Cafu");
+        // Abaixo está as outras 2 formas de criar um array de jogadores
+//        Jogador[] jogadores1 = new Jogador[3];
+//        Jogador[] jogadores1 = new Jogador[]{jogador1, jogador2, jogador3};
 
         Jogador[] jogadores = {jogador1,  jogador2, jogador3};
         //O array vai fazer referencia pra um mesmo objeto, que esta referenciando
