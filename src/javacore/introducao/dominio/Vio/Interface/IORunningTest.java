8f1bfb9373714f4ce12dcac0674748d22ce645e3
astonishiing <wcardoso4510@gmail.com>
2025-12-09 22:01:56 -0300

    IORunningTest

--- src/javacore/introducao/dominio/Vio/Interface/IORunningTest.java
+++ src/javacore/introducao/dominio/Vio/Interface/IORunningTest.java
@@ -1,4 +1,12 @@
 package javacore.introducao.dominio.Vio.Interface;
 
+import javacore.introducao.dominio.Vio.Interface.persistence.IOFilePersistence;
+
+import java.io.IOException;
+
 public class IORunningTest {
+    public static void main(String[] args) throws IOException {
+        FilePersistence persistence = new IOFilePersistence("user.txt");
+        System.out.println(persistence.write("Lucas;lucas@lucas.com;123456789;15/01/1990"));
+    }
 }
