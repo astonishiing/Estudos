5e812bc2b01b04c57481ab5ee1022912a6efe15f
astonishiing <wcardoso4510@gmail.com>
2025-12-10 14:11:12 -0300

    adicionando findAll e findBy

--- src/javacore/introducao/dominio/Vio/Interface/IORunningTest.java
+++ src/javacore/introducao/dominio/Vio/Interface/IORunningTest.java
@@ -6,7 +6,22 @@ import java.io.IOException;
 
 public class IORunningTest {
     public static void main(String[] args) throws IOException {
-        FilePersistence persistence = new IOFilePersistence("user.txt");
-        System.out.println(persistence.write("Lucas;lucas@lucas.com;123456789;15/01/1990"));
+        FilePersistence persistence = new IOFilePersistence("user.csv");
+        System.out.println("=======================");
+        System.out.println(persistence.write("Lucas;lucas@lucas.com;123456789;15/01/1990;"));
+        System.out.println(persistence.write("Maria;maria@maria.com;maria6789;23/10/2000;"));
+        System.out.println(persistence.write("Joao;joao@joao.com;joao789;05/09/1995;"));
+        System.out.println("=======================");
+        System.out.println(persistence.findAll());
+
+        System.out.println("=======================");
+        System.out.println(persistence.findBy("Lucas;"));
+        System.out.println("=======================");
+        System.out.println(persistence.findBy(";maria@"));
+        System.out.println("=======================");
+        System.out.println(persistence.findBy("95;"));
+        System.out.println("=======================");
+        System.out.println(persistence.findBy("22;"));
+
     }
 }
