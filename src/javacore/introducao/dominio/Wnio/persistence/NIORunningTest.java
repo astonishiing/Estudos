8f7f195e016d4754b7d3c6dbcb39ee57f0c626ef
astonishiing <wcardoso4510@gmail.com>
2025-12-11 18:03:20 -0300

    Criacao do executavel da Classe NIOFilePersistence

--- src/javacore/introducao/dominio/Wnio/persistence/NIORunningTest.java
+++ src/javacore/introducao/dominio/Wnio/persistence/NIORunningTest.java
@@ -1,4 +1,16 @@
 package javacore.introducao.dominio.Wnio.persistence;
 
 public class NIORunningTest {
+    public static void main(String[] args) throws Exception {
+        FilePersistence persistence = new NIOFilePersistence("user.csv");
+        System.out.println(persistence.write("bianca;bia@bia.com;22/09/1997;"));
+        System.out.println("=================");
+        System.out.println(persistence.write("bernardo;bernado@bernado.com;28/11/1999;"));
+        System.out.println("=================");
+        System.out.println(persistence.write("ricardo;ricardo@ricardo.com;12/01/2000;"));
+        System.out.println("=================");
+        System.out.println(persistence.findAll());
+        System.out.println("=================");
+
+    }
 }
