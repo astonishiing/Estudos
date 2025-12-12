3ebac6465e2dd8ce148866b6e5d343b8cd9572af
astonishiing <wcardoso4510@gmail.com>
2025-12-12 16:55:22 -0300

    Criando o executavel para a classe NIO2FilePersistence

--- src/javacore/introducao/dominio/Wnio/persistence/NIO2RunningTest.java
+++ src/javacore/introducao/dominio/Wnio/persistence/NIO2RunningTest.java
@@ -1,4 +1,9 @@
 package javacore.introducao.dominio.Wnio.persistence;
 
+import java.io.IOException;
+
 public class NIO2RunningTest {
+    public static void main(String[] args) throws IOException {
+        FilePersistence persistence = new NIO2FilePersistence("user.csv");
+    }
 }
