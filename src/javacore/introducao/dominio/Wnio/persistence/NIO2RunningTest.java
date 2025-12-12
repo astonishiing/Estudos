93756411e09f97e867789f91dc59fb34741b5648
astonishiing <wcardoso4510@gmail.com>
2025-12-12 17:07:57 -0300

    Adicionando executavel para NIO2FilePersistence

--- src/javacore/introducao/dominio/Wnio/persistence/NIO2RunningTest.java
+++ src/javacore/introducao/dominio/Wnio/persistence/NIO2RunningTest.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 
 public class NIO2RunningTest {
     public static void main(String[] args) throws IOException {
-        FilePersistence persistence = new NIO2FilePersistence("user.csv");
+        FilePersistence persistence = new NIO2FilePersistence("user.txt");
+        System.out.println("================");
+        System.out.println(persistence.write("Luana;luana@luana.com;28/09/1989;"));
+        System.out.println("================");
+        System.out.println(persistence.write("Marcos;marcos@marcos.com;01/01/1999;"));
+        System.out.println("================");
+        System.out.println(persistence.write("Henrique;henrique@henrique.com;03/05/2001;"));
     }
 }
