cb7e775afc8c500e901471973b1b5c8a19878282
astonishiing <wcardoso4510@gmail.com>
2025-12-12 17:46:24 -0300

    Criando Testes

--- src/javacore/introducao/dominio/Wnio/persistence/NIO2RunningTest.java
+++ src/javacore/introducao/dominio/Wnio/persistence/NIO2RunningTest.java
@@ -11,5 +11,21 @@ public class NIO2RunningTest {
         System.out.println(persistence.write("Marcos;marcos@marcos.com;01/01/1999;"));
         System.out.println("================");
         System.out.println(persistence.write("Henrique;henrique@henrique.com;03/05/2001;"));
+        System.out.println("================");
+        System.out.println(persistence.findAll());
+        System.out.println("================");
+        System.out.println(persistence.removeContent(";marcos@"));
+        System.out.println("================");
+        System.out.println(persistence.removeContent("Luiz"));
+        System.out.println("================");
+        System.out.println(persistence.findBy(";28/09"));
+        System.out.println("================");
+        System.out.println(persistence.findBy("Marcos"));
+        System.out.println("================");
+        System.out.println(persistence.replaceCont(";03/05", "Eric;eric@eric.com;20/06/1985;"));
+        System.out.println("================");
+        System.out.println(persistence.replaceCont("Julia", "Maria;maria@maria.com;09/09/1999"));
+        System.out.println("================");
+        System.out.println(persistence.findAll());
     }
 }
