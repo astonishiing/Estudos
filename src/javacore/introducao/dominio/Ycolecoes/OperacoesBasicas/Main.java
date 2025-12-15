d87646c6dd4f663f625979d0a01ee862b675caee
astonishiing <wcardoso4510@gmail.com>
2025-12-15 17:33:14 -0300

    Criacao do executavel para as listas

--- src/javacore/introducao/dominio/Ycolecoes/OperacoesBasicas/Main.java
+++ src/javacore/introducao/dominio/Ycolecoes/OperacoesBasicas/Main.java
@@ -1,4 +1,20 @@
 package javacore.introducao.dominio.Ycolecoes.OperacoesBasicas;
 
 public class Main {
+    public static void main(String[] args) {
+        ListaTarefa listaTarefa = new ListaTarefa();
+
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 1");
+        listaTarefa.adicionarTarefa("Tarefa 2");
+        listaTarefa.adicionarTarefa("Tarefa 3");
+        System.out.println("O número total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.removerTarefa("Tarefa 1");
+        System.out.println("O nuúmero total de elementos na lista é: " + listaTarefa.obterNumeroTotalTarefas());
+
+        listaTarefa.obterDescricoesTarefas();
+    }
 }
