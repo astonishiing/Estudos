fcf3a3670ef56e489d8ed37079e43e6f61df4aaa
astonishiing <wcardoso4510@gmail.com>
2025-12-15 17:33:42 -0300

    ListaTarefa

--- src/javacore/introducao/dominio/Ycolecoes/OperacoesBasicas/ListaTarefa.java
+++ src/javacore/introducao/dominio/Ycolecoes/OperacoesBasicas/ListaTarefa.java
@@ -1,4 +1,35 @@
 package javacore.introducao.dominio.Ycolecoes.OperacoesBasicas;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListaTarefa {
+    private List<Tarefa> tarefaList;
+
+
+    public ListaTarefa() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> tarefasRemove = new ArrayList<>();
+        for (Tarefa tarefa : tarefaList) {
+            if(tarefa.getDescricao().equalsIgnoreCase(descricao)){
+                tarefasRemove.add(tarefa);
+            }
+        }
+        tarefaList.removeAll(tarefasRemove);
+    }
+
+    public int obterNumeroTotalTarefas(){
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas(){
+        System.out.println(tarefaList);
+    }
 }
