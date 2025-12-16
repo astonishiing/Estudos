3ec25bc1391411bb4759c57ab992d1464546f088
astonishiing <wcardoso4510@gmail.com>
2025-12-15 21:15:00 -0300

    Criacao da classe CatalogoLivro

---
+++ src/javacore/introducao/dominio/Ycolecoes/Pesquisa/CatalogoLivro.java
@@ -0,0 +1,52 @@
+package javacore.introducao.dominio.Ycolecoes.Pesquisa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CatalogoLivro {
+
+    private List<Livro> livrosList;
+
+    public CatalogoLivro() {
+        this.livrosList = new ArrayList<>();
+    }
+
+    public void adicionarLivro(String titulo, String autor, int anoPublicacao){
+        livrosList.add(new Livro(titulo, autor, anoPublicacao));
+    }
+
+    public List<Livro> pesquisarPorAutor(String autor){
+        List<Livro> livrosPorAutor = new ArrayList<>();
+        if(!livrosList.isEmpty()){
+            for (Livro livro : livrosList) {
+                if(livro.getAutor().equalsIgnoreCase(autor)){
+                    livrosPorAutor.add(livro);
+                }
+            }
+        }
+        return livrosPorAutor;
+    }
+
+    public List<Livro> pesquisaPorIntervalosAnos(int anoInicial, int anoFinal){
+        List<Livro> livroPorIntervalos = new ArrayList<>();
+        for (Livro livro : livrosList) {
+            if(livro.getAnoPublicacao() >= anoInicial && livro.getAnoPublicacao() <= anoFinal){
+                livroPorIntervalos.add(livro);
+            }
+        }
+        return null;
+    }
+    public Livro pesquisarPorTitulo(String titulo){
+        Livro livroPorTitulo = null;
+        if(!livrosList.isEmpty()){
+            for (Livro livro : livrosList) {
+                if(livro.getTitulo().equalsIgnoreCase(titulo)){
+                    livroPorTitulo = livro;
+                    break;
+                }
+            }
+        }
+        return  livroPorTitulo;
+    }
+
+}
