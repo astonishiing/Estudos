339b8019c857a310d30fa396960d742821a4dcf1
astonishiing <wcardoso4510@gmail.com>
2025-12-16 17:05:36 -0300

    Criacao de um executavel

--- src/javacore/introducao/dominio/Ycolecoes/Set/OperacoesBasicas/Main.java
+++ src/javacore/introducao/dominio/Ycolecoes/Set/OperacoesBasicas/Main.java
@@ -1,4 +1,24 @@
 package javacore.introducao.dominio.Ycolecoes.Set.OperacoesBasicas;
 
 public class Main {
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+
+        System.out.println("Existem: " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+        conjuntoConvidados.adicionarElemento("Convidado 1", 1234);
+        conjuntoConvidados.adicionarElemento("Convidado 2", 1235);
+        conjuntoConvidados.adicionarElemento("Convidado 3", 1235);
+        conjuntoConvidados.adicionarElemento("Convidado 4", 1221);
+        conjuntoConvidados.adicionarElemento("Convidado 5", 1292);
+
+        conjuntoConvidados.exibirConvidados();
+
+        System.out.println("Existem: " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        System.out.println("Existem: " + conjuntoConvidados.contarConvidados() + " convidados dentro do Set de Convidados");
+
+        conjuntoConvidados.exibirConvidados();
+
+    }
 }
