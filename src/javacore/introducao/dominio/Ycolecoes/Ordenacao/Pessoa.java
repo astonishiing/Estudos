8193d08ed5779bfb118ee6069bf605d237e80ed1
astonishiing <wcardoso4510@gmail.com>
2025-12-15 22:02:07 -0300

    Criacao da classe Pessoa

---
+++ src/javacore/introducao/dominio/Ycolecoes/Ordenacao/Pessoa.java
@@ -0,0 +1,40 @@
+package javacore.introducao.dominio.Ycolecoes.Ordenacao;
+
+public class Pessoa implements Comparable<Pessoa> {
+    private String nome;
+
+    private int idade;
+    private double altura;
+
+    public Pessoa(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    @Override
+    public int compareTo(Pessoa o) {
+        return Integer.compare(idade, o.getIdade());
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{" +
+                "nome='" + nome + '\'' +
+                ", idade=" + idade +
+                ", altura=" + altura +
+                '}';
+    }
+}
