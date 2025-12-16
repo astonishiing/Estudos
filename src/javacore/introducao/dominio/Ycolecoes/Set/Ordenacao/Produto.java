8090c429efd973bff028492c4d6940ab4a6fc922
astonishiing <wcardoso4510@gmail.com>
2025-12-16 17:30:32 -0300

    Criacao da classe Produtos

---
+++ src/javacore/introducao/dominio/Ycolecoes/Set/Ordenacao/Produto.java
@@ -0,0 +1,56 @@
+package javacore.introducao.dominio.Ycolecoes.Set.Ordenacao;
+
+import java.util.Objects;
+
+public class Produto {
+
+    private String nome;
+    private long codigo;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, long codigo, double preco, int quantidade) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Produto produto = (Produto) o;
+        return codigo == produto.codigo;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(codigo);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", codigo=" + codigo +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
