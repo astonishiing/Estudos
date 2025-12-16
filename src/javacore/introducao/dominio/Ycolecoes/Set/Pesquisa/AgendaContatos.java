81b9ad4f423ae07238812775879796c96bf5c206
astonishiing <wcardoso4510@gmail.com>
2025-12-16 17:19:40 -0300

    Adicao da classe AgendaContatos

--- src/javacore/introducao/dominio/Ycolecoes/Set/Pesquisa/AgendaContatos.java
+++ src/javacore/introducao/dominio/Ycolecoes/Set/Pesquisa/AgendaContatos.java
@@ -1,4 +1,42 @@
 package javacore.introducao.dominio.Ycolecoes.Set.Pesquisa;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class AgendaContatos {
+    private Set<Contato> contatoSet;
+
+    public AgendaContatos() {
+        this.contatoSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int nummero){
+        contatoSet.add(new Contato(nome, nummero));
+    }
+
+    public void exibirContato(){
+        System.out.println(contatoSet);
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome){
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for (Contato contato : contatoSet) {
+            if(contato.getNome().startsWith(nome)){
+                contatosPorNome.add(contato);
+            }
+        }
+        return contatosPorNome;
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero){
+        Contato contatoAtualizado = null;
+        for (Contato c : contatoSet) {
+            if(c.getNome().equalsIgnoreCase(nome)){
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+    }
 }
