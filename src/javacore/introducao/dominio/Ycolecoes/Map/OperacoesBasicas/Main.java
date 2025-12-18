8adfacd29195e5baeea93f94bc42550e44d855c8
astonishiing <wcardoso4510@gmail.com>
2025-12-18 20:55:02 -0300

    test: Criar classe executavel

--- src/javacore/introducao/dominio/Ycolecoes/Map/OperacoesBasicas/Main.java
+++ src/javacore/introducao/dominio/Ycolecoes/Map/OperacoesBasicas/Main.java
@@ -1,4 +1,21 @@
 package javacore.introducao.dominio.Ycolecoes.Map.OperacoesBasicas;
 
 public class Main {
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Ana", 123456);
+        agendaContatos.adicionarContato("Ana", 5665);
+        agendaContatos.adicionarContato("Ana Cavalcante", 111111);
+        agendaContatos.adicionarContato("Ana Silva", 654987);
+        agendaContatos.adicionarContato("Duda Silva", 111111);
+        agendaContatos.adicionarContato("Ana", 44444);
+
+        agendaContatos.exibirContato();
+
+        agendaContatos.removerContato("Duda Silva");
+        agendaContatos.exibirContato();
+
+        System.out.println("O número é: " + agendaContatos.pesquisarPorNome("Ana Silva"));
+    }
 }
