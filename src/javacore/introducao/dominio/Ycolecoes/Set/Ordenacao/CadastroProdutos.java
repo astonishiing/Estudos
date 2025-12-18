0891f3e6958ec376ed1715db95c1ab162ca7dfaf
astonishiing <wcardoso4510@gmail.com>
2025-12-18 17:41:04 -0300

    Criacao da classe CadastroProduto

--- src/javacore/introducao/dominio/Ycolecoes/Set/Ordenacao/CadastroProdutos.java
+++ src/javacore/introducao/dominio/Ycolecoes/Set/Ordenacao/CadastroProdutos.java
@@ -1,4 +1,31 @@
 package javacore.introducao.dominio.Ycolecoes.Set.Ordenacao;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class CadastroProdutos {
+
+    private Set<Produto> produtoSet;
+
+    public CadastroProdutos() {
+        this.produtoSet = new HashSet<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, double preco, int quantidade) {
+        produtoSet.add(new Produto(nome, cod, preco, quantidade));
+    }
+
+    public Set<Produto> exibirProdutosPorNome() {
+        Set<Produto> produtosPorNome = new TreeSet<>(produtoSet);
+        return produtosPorNome;
+    }
+
+    public Set<Produto> exibirProdutorPorPreco() {
+        Set<Produto> produtosPorPreco = new TreeSet<>(new ComparatorPorPreco());
+        produtoSet.addAll(produtoSet);
+        return produtosPorPreco;
+    }
+
+
 }
