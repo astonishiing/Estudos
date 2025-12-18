e46cc1e97336d0aa57f91427ff7147ec746b2150
astonishiing <wcardoso4510@gmail.com>
2025-12-18 17:41:32 -0300

    Criacao da classe ComparatorPorPreco para comparacoes

--- src/javacore/introducao/dominio/Ycolecoes/Set/Ordenacao/ComparatorPorPreco.java
+++ src/javacore/introducao/dominio/Ycolecoes/Set/Ordenacao/ComparatorPorPreco.java
@@ -1,4 +1,11 @@
 package javacore.introducao.dominio.Ycolecoes.Set.Ordenacao;
 
-public class ComparatorPorPreco {
+import java.util.Comparator;
+
+public class ComparatorPorPreco implements Comparator<Produto> {
+
+    @Override
+    public int compare(Produto o1, Produto o2) {
+        return Double.compare(o1.getPreco(), o2.getPreco());
+    }
 }
