4298266d953de62ace81bf9f2f4885c84f34a05c
astonishiing <wcardoso4510@gmail.com>
2025-12-18 21:26:49 -0300

    feat: Criar classe EstoqueProduto

--- src/javacore/introducao/dominio/Ycolecoes/Map/Pesquisa/EstoqueProdutos.java
+++ src/javacore/introducao/dominio/Ycolecoes/Map/Pesquisa/EstoqueProdutos.java
@@ -1,4 +1,45 @@
 package javacore.introducao.dominio.Ycolecoes.Map.Pesquisa;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EstoqueProdutos {
+
+    private Map<Long, Produto> estoqueProdutosMap;
+
+    public EstoqueProdutos() {
+        this.estoqueProdutosMap = new HashMap<>();
+    }
+
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco) {
+        estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
+    }
+
+    public void exibirProduto() {
+        System.out.println(estoqueProdutosMap);
+    }
+
+    public double calculaValorTotalEstoque() {
+        double valorTotalEstoque = 0d;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for (Produto p : estoqueProdutosMap.values()) {
+                valorTotalEstoque += p.getQuantidade() * p.getPreco();
+            }
+        }
+        return valorTotalEstoque;
+    }
+
+    public Produto obterProdutoMaisCaro() {
+        Produto produtoMaisCaro = null;
+        double maiorPreco = Double.MIN_VALUE;
+        if (!estoqueProdutosMap.isEmpty()) {
+            for (Produto p : estoqueProdutosMap.values()) {
+                if (p.getPreco() > maiorPreco) {
+                    produtoMaisCaro = p;
+                }
+            }
+        }
+        return produtoMaisCaro;
+    }
+
 }
