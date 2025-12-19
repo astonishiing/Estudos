86aff5fe8df4a13fb11587874930cde182051588
astonishiing <wcardoso4510@gmail.com>
2025-12-19 17:37:38 -0300

    feat: Criar classe AgendaEvento

--- src/javacore/introducao/dominio/Ycolecoes/Map/Ordenacao/AgendaEvento.java
+++ src/javacore/introducao/dominio/Ycolecoes/Map/Ordenacao/AgendaEvento.java
@@ -1,4 +1,43 @@
 package javacore.introducao.dominio.Ycolecoes.Map.Ordenacao;
 
+import java.time.LocalDate;
+import java.util.*;
+
 public class AgendaEvento {
+
+    private Map<LocalDate, Evento> eventoMap;
+
+    public AgendaEvento() {
+        this.eventoMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate date, String nome, String atracao) {
+        Evento evento = new Evento(nome, atracao);
+        eventoMap.put(date, evento);
+    }
+
+    public void exibirAgenda() {
+        Map<LocalDate, Evento> eventoTreeMap = new TreeMap<>(eventoMap);
+        System.out.println(eventoTreeMap);
+    }
+
+    public void obterProximoEvento() {
+        // O que há de semelhante com eles é, que nenhum deles se conhecem; Elas são coleções independentes
+        //Set<LocalDate> localDates = eventoMap.keySet(); // Ele está me retornando um set com todas as Keys
+        //Collection<Evento> values = eventoMap.values(); // Me retornar uma coleção com todos os valores
+
+        LocalDate now = LocalDate.now();
+        LocalDate afterDate = null;
+        Evento proximoEvento = null;
+
+        Map<LocalDate, Evento> eventoTreeMap = new TreeMap<>(eventoMap);
+        for (Map.Entry<LocalDate, Evento> entry : eventoTreeMap.entrySet()) {
+            if(entry.getKey().isEqual(now) || entry.getKey().isAfter(now)) {
+                afterDate = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O próximo evento: " + proximoEvento + " acontecerá na data " + afterDate);
+                break;
+            }
+        }
+    }
 }
