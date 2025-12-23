a6cb3ff8363aba80d6c5379d879f397e8a4f16d8
astonishiing <wcardoso4510@gmail.com>
2025-12-22 22:07:20 -0300

    add: adicionar novo lambda e adicionar Consumer a stream

--- src/javacore/introducao/dominio/ZZEstreams/Consumer/ConsumerExample.java
+++ src/javacore/introducao/dominio/ZZEstreams/Consumer/ConsumerExample.java
@@ -7,16 +7,34 @@ import java.util.function.Consumer;
 public class ConsumerExample {
     public static void main(String[] args) {
         //Criar uma lista de números inteiros
-        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
         //Usar o Consumer com expressões lambda para imprimir números pares
         Consumer<Integer> imprimirNumeroPar = numero -> {
-            if(numero % 2 == 0) {
+            if (numero % 2 == 0) {
                 System.out.println(numero);
             }
         };
 
         //Usar o Consumer para imprimir números pares no Stream
-        numeros.forEach(imprimirNumeroPar);
+        // 1° de fazer
+//        numeros.forEach(imprimirNumeroPar);
+        // 2° de fazer
+
+//        numeros.forEach(new Consumer<Integer>() {
+//            @Override
+//            public void accept(Integer integer) {
+//                if(integer % 2 == 0) {
+//                    System.out.println(integer);
+//                }
+//            }
+//        });
+
+        // 3° de fazer
+        numeros.forEach(integer -> {
+            if (integer % 2 == 0) {
+                System.out.println(integer);
+            }
+        });
     }
 }
