7d0358d0b1950e261d2ef06d3eaeff315b3ab163
astonishiing <wcardoso4510@gmail.com>
2025-12-22 21:55:53 -0300

    directory: criar nova pasta

---
+++ src/javacore/introducao/dominio/ZZEstreams/Consumer/ConsumerExample.java
@@ -0,0 +1,22 @@
+package javacore.introducao.dominio.ZZEstreams.Consumer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        //Criar uma lista de números inteiros
+        List<Integer> numeros = Arrays.asList(1,2,3,4,5);
+
+        //Usar o Consumer com expressões lambda para imprimir números pares
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if(numero % 2 == 0) {
+                System.out.println(numero);
+            }
+        };
+
+        //Usar o Consumer para imprimir números pares no Stream
+        numeros.forEach(imprimirNumeroPar);
+    }
+}
