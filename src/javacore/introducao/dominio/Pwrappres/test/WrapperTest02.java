0e8871860c49c0b90d257b008c0409e85ad5fc92
astonishiing <wcardoso4510@gmail.com>
2026-01-05 09:32:54 -0300

    feat: Adicionando executalve

--- src/javacore/introducao/dominio/Pwrappres/test/WrapperTest02.java
+++ src/javacore/introducao/dominio/Pwrappres/test/WrapperTest02.java
@@ -1,4 +1,19 @@
 package javacore.introducao.dominio.Pwrappres.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WrapperTest02 {
+    public static void main(String[] args) {
+        User user = new User("João", 20);
+
+        printValue(user);
+        System.out.println(user);
+    }
+
+    private static void printValue(User user) {
+        user = new User("Maria" , 33);
+        System.out.println(user);
+
+    }
 }
