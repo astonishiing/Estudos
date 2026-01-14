0f224ccebd240e704b49a213398dc3c6947d5b1b
astonishiing <wcardoso4510@gmail.com>
2026-01-14 10:30:38 -0300

    feat: Adicionar a Classe DateTimeTest01

--- src/javacore/introducao/dominio/Rdates/test/DateTimeTest01.java
+++ src/javacore/introducao/dominio/Rdates/test/DateTimeTest01.java
@@ -1,4 +1,31 @@
 package javacore.introducao.dominio.Rdates.test;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateTimeTest01 {
+    public static void main(String[] args) {
+        long currentTimeMillis = System.currentTimeMillis();
+        Date dateMillis = new Date(currentTimeMillis);
+
+        Date date = new Date();
+
+        Date date2 = new Date(System.currentTimeMillis() - 999999L);
+        DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy - HH:mm:ss");
+
+        System.out.println(date);
+        System.out.println(dateFormat.format(date));
+
+        System.out.println("============");
+        System.out.println(date2);
+
+        System.out.println("=============");
+        System.out.println(dateMillis.before(new Date(currentTimeMillis + 99999)));
+        System.out.println(new Date(currentTimeMillis + 99999).after(dateMillis));
+
+
+    }
+
+
 }
