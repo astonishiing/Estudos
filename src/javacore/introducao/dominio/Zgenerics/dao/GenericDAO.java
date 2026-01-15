576f596853af27699605404a039df82790839134
astonishiing <wcardoso4510@gmail.com>
2026-01-15 19:12:28 -0300

    feat: Adicionar classe Abstrata GenericDAO

--- src/javacore/introducao/dominio/Zgenerics/dao/GenericDAO.java
+++ src/javacore/introducao/dominio/Zgenerics/dao/GenericDAO.java
@@ -1,4 +1,48 @@
 package javacore.introducao.dominio.Zgenerics.dao;
 
-public class GenericDAO {
+import javacore.introducao.dominio.Zgenerics.domain.GenericDomain;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Predicate;
+
+public abstract class GenericDAO<ID, T extends GenericDomain<ID>> {
+    private final List<T> db = new ArrayList<>();
+
+    private T save(T domain) {
+        db.add(domain);
+        return domain;
+    }
+
+    public boolean save(int batch,T... domains) {
+        System.out.printf("Salvando em lote(%s) \n", batch);
+        return db.addAll(Arrays.stream(domains).toList());
+    }
+
+    public T update(ID id, T domain) {
+        T stored = find(t -> t.getId().equals(id)).orElseThrow();
+
+        db.remove(stored);
+        db.add(domain);
+        return save(domain);
+    }
+
+    public boolean delete(T domain) {
+        return db.remove(domain);
+    }
+
+    public Optional<T> find(Predicate<T> filterCAllBack) {
+        return db.stream().filter(filterCAllBack).findFirst();
+    }
+
+    public List<T> findAll() {
+        return db;
+    }
+
+    public int count() {
+        return db.size();
+    }
+
 }
