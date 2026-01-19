2416ae3529d1b848a21303d068d37fda8eb4e417
astonishiing <wcardoso4510@gmail.com>
2026-01-19 15:06:44 -0300

    test: Adicionar Executavel AtomiIntegerTest01

--- src/javacore/introducao/dominio/ZZFthreads/test/AtomicIntegerTest01.java
+++ src/javacore/introducao/dominio/ZZFthreads/test/AtomicIntegerTest01.java
@@ -1,4 +1,35 @@
 package javacore.introducao.dominio.ZZFthreads.test;
 
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AtomicIntegerTest01 {
+    private final static AtomicInteger number = new AtomicInteger(0);
+
+    public static void main(String[] args) {
+        Runnable inc = () -> {
+            for (int i = 0; i < 1000_000; i++) {
+                number.incrementAndGet();
+            }
+        };
+
+        Runnable dec = () -> {
+            for (int i = 0; i < 1000_000; i++) {
+                number.decrementAndGet();
+            }
+        };
+
+        Runnable show = () -> {
+            for (int i = 0; i < 1000_000; i++) {
+                System.out.println(number);
+            }
+        };
+
+        var execInt = new Thread(inc);
+        execInt.start();
+        var execDec = new Thread(dec);
+        execDec.start();
+        var execShow = new Thread(show);
+        execShow.start();
+    }
 }
